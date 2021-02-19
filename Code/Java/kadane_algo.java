32e0eb551c4c5ee59668b125a7aa5ae6a2543e23
ganesspandian2 <ganesspandian2@gmail.com>
2021-02-19 18:52:48 +0530

    added Kadane Algorithm in java

--- Code/Java/kadane_algo.java
+++ Code/Java/kadane_algo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class kadane_algo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
