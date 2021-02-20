c75b1a70307344677f3381422409463b8b067811
Abhik Jain <abhikjain360@gmail.com>
2021-02-20 08:08:48 +0530

    formatting fix

--- Code/Rust/src/main.rs
+++ Code/Rust/src/main.rs
@@ -1,8 +1,8 @@
 use clap::clap_app;
 
 // importing all the algorithms
-use wave_print;
 use spiral_print;
+use wave_print;
 
 const ALGOS: &'static str = r#"
 spiral print
@@ -11,10 +11,11 @@ wave print
 
 pub fn main() {
     let matches = clap_app!(AlgoTree =>
-                            (version: "0.1.0")
-                            (about: "Run all the algorithms!")
-                            (@arg ALGO: +required "tell the algorithm to use")
-        ).get_matches();
+                        (version: "0.1.0")
+                        (about: "Run all the algorithms!")
+                        (@arg ALGO: +required "tell the algorithm to use")
+    )
+    .get_matches();
 
     match matches.value_of("ALGO").expect(matches.usage()) {
         "spiral_print" | "spiral print" => spiral_print::runner().unwrap(),
