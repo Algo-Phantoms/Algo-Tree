184bba2d36815df38d8e17da7f7b96b1d024d741
Jahnavi Chennamsetty <63448333+jahnavi0105@users.noreply.github.com>
2021-03-03 07:29:16 +0530

    Created insertion_at_positio.cpp

---
+++ Code/C++/insertion_at_position.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    int n;
+    cin>>n;
+
+    int array[1000];
+
+    for(int i=0;i<n;i++){
+    cin>>array[i];
+    }
+
+    int element;
+    cin>>element;
+
+    cout<<"Initial array:\n";
+    for(int i=0;i<n;i++){
+    cout<<array[i]<<" ";
+    }
+    cout<<"\n";
+
+    int position;
+    cin>>position;
+
+    for(int i=n-1;i>=position-1;i--){
+      array[i+1]=array[i];
+    }
+
+    array[position-1]=element;
+
+    cout<<"Final array:\n";
+    for(int i=0;i<=n;i++){
+      cout<<array[i]<<" ";
+    }
+    return 0;
+}
+/*
+  Input : 5
+  1 2 3 4 5
+  6-element
+  Output : 
+  Initial array 
+  1 2 3 4 5
+  3-position
+  Final array 
+  1 2 6 3 4 5
+  Time complexity:O(n)
+  Space complexity:O(n)
+*/
