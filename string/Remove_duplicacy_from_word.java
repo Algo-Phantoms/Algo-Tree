f7bb8379554e39618a9ac07ca5a4ffcc7f5c98ff
AkanshaUniyal <78859329+AkanshaUniyal@users.noreply.github.com>
2021-03-03 09:45:17 +0530

    Update Remove_duplicacy_from_word.java

--- string/Remove_duplicacy_from_word.java
+++ string/Remove_duplicacy_from_word.java
@@ -1,47 +1,56 @@
-
-import java.util.*;  // Calling the util package
+package mypack;
+import java.util.*; 
  public class Remove {
-   
-    
+
+
  /*recursive function calduplicacy() eliminates duplicate characters by adding characters to the new string if a new character is found in the 0th index of original string.
  The original string is then trimmed by deleting the first character each time till it becomes null.*/
+ public int recur;
+ String newstring;
+
+ Remove()
+ {
+ recur=0;
+ newstring="";
+ } 
 
 
-   public void calduplicacy(String original,String nestring)  
-{
+   public void calduplicacy(String original)  
+ { 	
     if(original.equals(""))
     {
-    System.out.println("After removal of duplicate characters: "+nestring);
-    
+    System.out.println("After removal of duplicate characters: "+newstring);
+
     }
     else{
-        if(nestring.equals(""))
-       { nestring=original.charAt(0)+nestring;}
+        if(recur==0)
+       { newstring=original.charAt(0)+newstring;}
         else{
             char ch=original.charAt(0);int count=0;
-            for(int charcount=0;charcount<nestring.length();charcount++)
+            for(int charcount=0;charcount<newstring.length();charcount++)
             {
-                if(ch==nestring.charAt(charcount)||ch==(char)(nestring.charAt(charcount)+32)||ch==(char)(nestring.charAt(charcount)-32))
+                if(ch==newstring.charAt(charcount)||ch==(char)(newstring.charAt(charcount)+32)||ch==(char)(newstring.charAt(charcount)-32))
                 count++;
             }
             if(count==0)
-            nestring=nestring+ch;
+            newstring=newstring+ch;
         }
-            calduplicacy(original.substring(1),nestring);
+            recur++; calduplicacy(original.substring(1));
     }
 }
-   
+
     public static void main(String args[])
     {
         Scanner scan=new Scanner(System.in);
-        
+
         System.out.println("Enter a word");
         String input=scan.nextLine();
         Remove d=new Remove();
-        d.calduplicacy(input,"");
+        d.calduplicacy(input);
     }
-    
+
 }
+
 
 /* 		OUTPUT:
 
