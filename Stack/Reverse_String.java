23b72a02e2034163c985e606c182b7056ff379ab
PRATEEK NAIR <56732496+prateek-jpg@users.noreply.github.com>
2021-03-04 10:33:32 +0530

    Update Reverse_String.java

--- Stack/Reverse_String.java
+++ Stack/Reverse_String.java
@@ -11,29 +11,29 @@ public class Reverse_String {
 
 	public static void main(String[] args) {
 		
-				Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);
 		
         System.out.print("Enter a String:");
         
-				String s=sc.nextLine();												//Enter the string you want to reverse
+	String s=sc.nextLine();												//Enter the string you want to reverse
 
-        int l=s.length();															//Storing the length of the string
+        int l=s.length();												//Storing the length of the string
 
-        Stack<Character> a=new Stack<>();             //Creating a stack named "a" 
+        Stack<Character> a=new Stack<>();             									//Creating a stack named "a" 
 
        
 
         for(char ch:s.toCharArray()){			
 																											//iterating through the string using forEach loop
-            a.push(ch);																//adding each character to the stack
+            a.push(ch);													//adding each character to the stack
         
-				}
+	}
 
         s="";
 
         while(l-->0){
 					
-            s=s+a.pop();															//as pop occurs from top therefore while popping a character we add it to a string which is stored in reverse order 
+            s=s+a.pop();												//as pop occurs from top therefore while popping a character we add it to a string which is stored in reverse order 
 					
         }
 		
