20d8f1a185e32c341733737f54b4f79653cfcca4
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-03-08 15:50:19 +0530

    Class name same as file name Resolved

--- Code/Java/factorial.java
+++ Code/Java/factorial.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        intgit  n = sc.nextInt();
+        int n = sc.nextInt();
         System.out.println(fact(n));
     }
     public static int fact(int n){
