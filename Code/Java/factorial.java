278f9253d54acafbe82d31fe9bc61c7f0e207a32
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-03-08 15:48:21 +0530

    Prime File Removed

--- Code/Java/factorial.java
+++ Code/Java/factorial.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        intgit  n = sc.nextInt();
         System.out.println(fact(n));
     }
     public static int fact(int n){
