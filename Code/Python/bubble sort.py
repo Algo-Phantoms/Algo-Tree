851bf2c361a7c74698fc8b688726a16f29ba3a6a
Sukhpreet Singh <72423196+sukhpreet-singh1@users.noreply.github.com>
2021-03-08 21:37:25 +0530

    Update bubble sort.py

--- Code/Python/bubble sort.py
+++ Code/Python/bubble sort.py
@@ -1,4 +1,5 @@
-#created bubble sort function with array parameter
+# Created bubble sort function with array parameter
+# Description : It works by repeatedly swapping the adjacent elements if they are in wrong order.One by One element are placed at rigth position
 def bubbleSort(arr): 
 	n=len(arr) 
 	# Traverse through array elements 
@@ -11,3 +12,12 @@ def bubbleSort(arr):
                 		arr[j], arr[j+1] = arr[j+1], arr[j] 
                 		
   
+# Time complexity 
+# Worst case : O(n*n) when array is sorted in reverse order	
+# Best case  : O(n)   when array is already sorted order
+# Space complexity : O(1)
+# Test cases : 
+# Input:  5 4 6 7 3 2 1
+# Output: 1 2 3 4 5 6 7
+# Input:  23 11 65 100 40 200
+# Output: 11 23 40 65 100 200
