d98ed8796cf85f2ad431cf760af98f6406aee33d
Ekjot Kaur <ekjotkaurk13@gmail.com>
2021-03-10 09:53:58 +0530

    comments added and file location

--- Code/C++/shell_sort.cpp
+++ Code/C++/shell_sort.cpp
@@ -5,27 +5,37 @@
 
 */
 
-
 #include <iostream>
 
 using namespace std;
 
-
 void ShellSort(int A[], int n)
 {
-    int gap,i,j,temp;
-    for(gap=n/2;gap>=1;gap/=2)
+    int gap, i, j, temp;
+
+    // Start with a big gap, then reduce the gap 
+    for (gap = n / 2; gap >= 1; gap /= 2)
     {
-        for(i=gap;i<n;i++)
+        // Do a gapped insertion sort for this gap size. 
+        // The first gap elements a[0..gap-1] are already in gapped order 
+        // keep adding one more element until the entire array is gap sorted  
+        for (i = gap; i < n; i++)
         {
-            temp=A[i];
-            j=i-gap;
-            while(j>=0 && A[j]>temp)
+            // add a[i] to the elements that have been gap sorted 
+            // save a[i] in temp and make a hole at position i 
+            temp = A[i];
+            j = i - gap;
+
+            // shift earlier gap-sorted elements up until the correct  
+            // location for a[i] is found 
+            while (j >= 0 && A[j] > temp)
             {
-                A[j+gap]=A[j];
-                j=j-gap;
+                A[j + gap] = A[j];
+                j = j - gap;
             }
-            A[j+gap]=temp;
+
+            //  put temp (the original a[i]) in its correct location 
+            A[j + gap] = temp;
         }
     }
 }
@@ -33,26 +43,24 @@ void ShellSort(int A[], int n)
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
 
     int A[n];
 
-    for(int i=0; i<n ; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>A[i];
+        cin >> A[i];
     }
 
     ShellSort(A, n);
 
-    for(int i=0; i<n ; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout<<A[i]<<" ";
+        cout << A[i] << " ";
     }
 
     return 0;
 }
-
-
 
 /*
 
@@ -81,4 +89,3 @@ int main()
     Space Complexity: O(1)
 
 */
-
