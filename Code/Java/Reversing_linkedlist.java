cba183067eec9eb97405f9937d136ff0b0636fde
pallavidhere23 <pallavidhere23@gmail.com>
2021-03-10 15:44:30 +0530

    Second commit

--- Code/Java/Reversing_linkedlist.java
+++ Code/Java/Reversing_linkedlist.java
@@ -1,4 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author Pallavi Dhere
+ *
+ */
+package Reversinglinkedlist;
+
+
 /*Here Pointer to the head node of a linked list is given, 
+
  * the task is to reverse the linked list. We are going to reverse the list by changing the links between nodes.We are using Iterative method.*/
 /*Initialize three pointers prev as NULL, curr as head and next as NULL.
           Iterate through the linked list. In loop, do following. 
@@ -16,69 +27,70 @@
 import java.util.*;   //used for Scanner function
 import java.io.*;     //used for IOException
 
-class Node{                    //creating Node class 
-    int data;
-    Node next;
-    
-    Node(int x){
-        data = x;
-        next = null;
-    }
-    
-}
-public class Reversing_linkedlist {
 
+public class Reversing_Linkedlist {
+            
+    
+    static class Node{                    //creating Node class 
+         public int data;
+         public  Node next;
+        
+            Node(int x){
+              this.data = x;
+              this.next = null;
+              }
+        
+           }
 
-                 static void printList(Node node)   //Function to Print LinkedList
+    public static void printList(Node d)   //Function to Print LinkedList
+          { 
+              while (d != null) 
                  { 
-	                   while (node != null) 
-	                   { 
-		                   System.out.print(node.data + " "); 
-		                   node = node.next;                         //referring to next node
-	                   } 
-	                System.out.println(); 
-                  } 
-         public static void main(String args[]) throws IOException {          //Main starts
+                    System.out.print(d.data + " "); 
+                    d = d.next;                         //referring to next node
+                 } 
+                    System.out.println(); 
+           } 
+               
+    public static void main(String args[])  {          //Main starts
     
-                        Scanner sc =new Scanner(System.in);                  //Class used for input taking
-                        System.out.println("Enter The Numbers Of Nodes in LinkedList:");
-    	                int n = sc.nextInt();                                //Number of nodes
+             Scanner sc =new Scanner(System.in);                  //Class used for input taking
+             System.out.println("Enter The Numbers Of Nodes in LinkedList:");
+             int n = sc.nextInt();                                //Number of nodes
     
-                        Node head = new Node(sc.nextInt());
-                        Node tail = head;
+             Node head = new Node(sc.nextInt());
+             Node tail = head;
     
-                        for(int i=0; i<n-1; i++)                              //For Loop for taking Input
-                        {
-                             tail.next = new Node(sc.nextInt());
-                             tail = tail.next;
-                        }
+             for(int i=0; i<n-1; i++)                              //For Loop for taking Input
+                {
+                  tail.next = new Node(sc.nextInt());
+                               
+                  tail = tail.next;
+               }
                           
-                          System.out.println("Entered LinkedList is:");
-                          printList(head);  
-                          Solution ob = new Solution();                       // object creation for solution class
-                          head = ob.reverseList(head);                         //using ob object reverseList is called
-                          System.out.println("Reversed LinkedList is:");
-                          printList(head); 
-                         
-            } 
- }
-class Solution{
-  
-    Node reverseList(Node head)                 //Function for reversing
-    {  
-    	Node current = head;
-        Node prev = null;
-        while(current!=null){                       //Checking if the linked list is not null
-        head = current.next;                        //assigning next of current to head
-        current.next = prev;                        //next of current node = prev;
-        prev = current;                             //prev is equal to current node
-        current = head;                             //current node is equal to head
+             System.out.println("Entered LinkedList is:");
+             printList(head);  
+                                                
+             System.out.println("Reversed LinkedList is:"+ reverseList(head));
+             printList(head); 
+                          
        }
-    
-  return prev;                                      
-      
-   }
-}
+             public static Node reverseList(Node head) {
+           
+                     Node current = head;
+                     Node prev = null;
+                     while(current!=null){                       //Checking if the linked list is not null
+                     head = current.next;                        //assigning next of current to head
+                     current.next = prev;                        //next of current node = prev;
+                     prev = current;                             //prev is equal to current node
+                     current = head;                             //current node is equal to head
+                 }
+        
+             return prev; 
+            
+        } 
+ }
+
 
 /*INPUT AND OUTPUT*/
 /*
