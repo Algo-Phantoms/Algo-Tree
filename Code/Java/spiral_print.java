b12abb3adb7eff307efbbeecb5b925e3bfe8e625
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2021-03-11 10:39:44 +0530

    Create spiral_print.java

--- Code/Java/spiral_print.java
+++ Code/Java/spiral_print.java
@@ -1,8 +1,6 @@
-import java.lang.*;
 import java.util.*;
-import java.io.*;
 
-public class Main
+public class spiral_array
 {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
