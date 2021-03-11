6bc629b029556e9a00b2e6dc8e56a6d0913343bd
vedant_jain03 <vedantjainben10@gmail.com>
2021-03-11 21:20:40 +0530

    Update Cycle detection in linked list.py

--- Code/Python/Cycle detection in linked list.py
+++ Code/Python/Cycle detection in linked list.py
@@ -1,6 +1,16 @@
 #Program to Detect loop in Linked List;
 #Algorithm is Floyd-Warshall Algorithm
 
+# #The Algorithm just uses two pointer approach
+
+# In this Algorithm we just initailize two pointer named as slow_pointer and fast_pointer and assign head of the linked list for traversing in the list then we assign slow to its next and fast to fast.next.next and we just check wheather slow and fast pointer meet at some point if yes their is cycle and if they end up with None thier is no cycle.
+
+# Step 1: Initialize slow_pointer and fast_pointer to head.next
+# Step 2: if slow_pointer, fast_pointer and fast_pointer.next is not none then we enter into loop and assign slow_pointer=slow_pointer.next and fast_pointer=fast_pointer.next.next
+#Step 3:In every case of loop we just check wheather fast_pointer and slow_pointer meet if yes we break the statement and loop and return true statement about detect loop
+
+
+
 #creating node class 
 class Node:
     #creating constructor to create new node and assign data with new data and next with None
@@ -51,9 +61,6 @@ else:
     print("Loop not detected")
     createLinkedList.printLinkedList()
     
-    
-#time Complexity:O(n)
-#space complexity:O(1)
 
 #test case 1:
 # createLinkedList.push(12)
@@ -76,3 +83,7 @@ else:
 # createLinkedList.head.next.next.next=createLinkedList.head
 # output:
 #     Loop detect
+
+    
+#time Complexity:O(n)
+#space complexity:O(1)
