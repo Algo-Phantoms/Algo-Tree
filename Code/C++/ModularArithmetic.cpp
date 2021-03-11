b7e4330beb81d70f6fccc9fbd74f40da8c271d7c
Rutuja Deshmukh <65710803+rutuja2229@users.noreply.github.com>
2021-03-11 17:57:12 +0530

    Update ModularArithmetic.cpp

--- Code/C++/ModularArithmetic.cpp
+++ Code/C++/ModularArithmetic.cpp
@@ -1,3 +1,38 @@
+/* Program that performs ADDDTION,SUBTRACTION, MULTIPLICATION, DIVISION, POWER.
+so, we are taking inputs 'a' and 'b' for above operaitions and also given MOD 'm'.
+All of them have constraints 1 to 10^50000
+
+Explaining the main points- 
+
+1 .  For performing DIVISION (a/b) we have to calculate the multiplicative inverse of 'b' first. The value of x should be in { 1, 2, … m-1}, i.e., in the range of integer 
+modulo m. ( Note that x cannot be 0 as a*0 mod m will never be 1 )
+The multiplicative inverse of “a modulo m” exists if and only if a and m are relatively prime (i.e., if gcd(a, m) = 1).
+
+2.  As our inputs are larger (10^50000) we cannot store them using long long int or any other datatype, so we are going to store them in string and convert it into 
+long long int datatybe, after gradually and simulteneously calulating modulo operation.
+
+3.  For above,  According to Feramts Little Theorem  
+         a^(p-1) mod p   =  1, When p is prime.
+   From this, as of the problem, M is prime, express A^B mod M as follows: 
+          A^B mod M = ( A^(M-1) * A^(M-1) *.......* A^(M-1) * A^(x) ) mod M          
+   Where x is B mod M-1 and A ^ (M-1) continues B/(M-1) times
+   Now, from Fermat’s Little Theorem,  
+           A ^ (M-1) mod M = 1.
+           A^B mod M = ( 1 * 1 * ....... * 1 * A^(x) ) mod M
+   Hence mod B with M-1 to reduce the number to a smaller one and then use power() method to compute (a^b)%m. 
+            
+   
+TIME COMPLEXITY - 
+        to convert inputs into modular value it will take bits 500000*log(10) approx O(1).
+        
+        to calculate  addition subtraction multiplication the time complexity is O(1).
+        to calculate division and power it will take O(Log m) time complexity, where is modulo given, Since after converting long input string to a number it will be under 'm'.
+
+*/
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,21 +42,10 @@ typedef unsigned long long ull;
 #define MOD 1000000007
 #define ftio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); //This is just used for fast input output operations
 
-/* Program that performs ADDDTION,SUBTRACTION, MULTIPLICATION, DIVISION, POWER.
-so, we are taking inputs 'a' and 'b' for above operaitions and also given MOD 'm'.
-All of them have constraints 1 to 10^50000
-*/
-
 ll m,md;
 ll gcd(ll a,ll b,ll *x,ll *y); // Function declaration
 
-
-
-/*
-The value of x should be in { 1, 2, … m-1}, i.e., in the range of integer modulo m. ( Note that x cannot be 0 as a*0 mod m will never be 1 )
-The multiplicative inverse of “a modulo m” exists if and only if a and m are relatively prime (i.e., if gcd(a, m) = 1).
-*/
-ll inverse(ll b,ll m){   // For performing DIVISION (a/b) we have to calculate the multiplicative inverse of 'b' first.
+ll inverse(ll b,ll m){   // see point 1
     ll x,y;
     ll g=gcd(b,m,&x,&y);
     if (g != 1) 
@@ -53,11 +77,8 @@ ll gcd1(ll a, ll b)
     return gcd1(b, a % b);  
       
 } 
-/* As our inputs are larger (10^50000) we cannot store them using long long int 
-or any other datatype, so we are going to store them in string and convert it into
-long long int datatybe, after gradually and simulteneously calulating modulo operation.
-*/ 
-ll modm(string s){
+
+ll modm(string s){ // see point 2
      ll number = 0; 
     for (ll i = 0; i < s.length(); i++) 
     { 
@@ -98,23 +119,8 @@ int main()
              
             a=modm(x);
             md=m;
-            m-=1;
-            /*
-            For above,  According to Feramts Little Theorem  
-                a^(p-1) mod p   =  1, When p is prime.
-            From this, as of the problem, M is prime, express A^B mod M as follows: 
-
-                     A^B mod M = ( A^(M-1) * A^(M-1) *.......* A^(M-1) * A^(x) ) mod M
+            m-=1;   // see point 3
             
-            Where x is B mod M-1 and A ^ (M-1) continues B/(M-1) times
-            Now, from Fermat’s Little Theorem,  
-                    A ^ (M-1) mod M = 1.
-                    
-            A^B mod M = ( 1 * 1 * ....... * 1 * A^(x) ) mod M
-            Hence mod B with M-1 to reduce the number to a smaller one and then use power() 
-            method to compute (a^b)%m. 
-            
-            */
             b=modm(y);
             m=md;
             if (a == 0) cout<<0<<"\n"; 
@@ -155,21 +161,32 @@ int main()
 
 
 /*
-Sample Input  
-6
+Sample Input 1 
+5
 + 2 3 2
 - 14 6 3
 * 11 3 5
 / 9 4 7
 ^ 3 3 11
+Sample Input 2
+5
++ 20986 303835 298
+- 148765 60000 393
+* 11937 3973632 59
+/ 19 4 5
 ^ 100000000000 1000009875 7
 
-Output
+Output 1
 1
 2
 3
 4
 5
+Ouput 2
+1
+340
+12
+1
 6
 */
 
