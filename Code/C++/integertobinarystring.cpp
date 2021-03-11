b74fa51e320ddb9d409d8d2b76e4844899bfe5ad
Vaibhav singh rawat <vaibhavsinghii1919@gmail.com>
2021-03-11 09:59:53 +0530

    Create integertobinarystring.cpp

---
+++ Code/C++/integertobinarystring.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;        
+
+string decimalToBinary(ll n) 
+{      
+    string s = bitset<64> //finding the binary form of the number
+    (n).to_string(); //stl used to convert a number into string 
+      
+    //Finding the first occurance of "1" 
+    //to strip off the leading zeroes. 
+    const auto loc1 = s.find('1'); 
+      
+    if(loc1 != string::npos) 
+        return s.substr(loc1); 
+      
+    return "0"; 
+} 
+
+
+int main() {
+    cout<<"Enter a binary number ";
+    int N;
+    cin>>N;
+    cout<<"\nIts binary string is "<<decimalToBinary(N);
+    return 0;
+}
