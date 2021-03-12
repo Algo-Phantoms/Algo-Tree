d4c805f8d61cd6d73d1e39fb0fe2d8f8bf3555f6
arnab031 <51916075+arnab031@users.noreply.github.com>
2021-03-12 19:40:02 +0530

    Update BST Traversal.py
    Second Commit

--- Code/Python/BST Traversal.py
+++ Code/Python/BST Traversal.py
@@ -1,42 +1,41 @@
-# Traversal is a process to visit all the nodes of a tree and may print their values too. Because all nodes are connected 
-# via edges(links) we always start from the root(head) node. So, we cannot randomly access a node in a tree.
-#
+# Traversal is a process to visit all the nodes of a tree and may print their values too. Because all nodes are connected via edges(links) we always start from the root(head) node. So, we cannot randomly access a node in a tree.
 # There are three ways which we use to traverse a tree −
 # Inorder Traversal:
 # 1.Traverse the left subtree.
 # 2.Visit the root.
 # 3.Traverse the right subtree.
-#
 # Preorder Traversal:
 # 1.Visit the root.
 # 2.Traverse the left subtree.
 # 3.Traverse the right subtree.
-#
 # Postorder Traversal:
 # 1.Traverse the left subtree.
 # 2.Traverse the right subtree.
 # 3.Visit the root.
 
 class Node:
-    #Initialization of Node
     def __init__(self,key):
+        """Initialization of Node"""
         self.left=None
         self.right=None
         self.val=key
 
 def preOrder(root):
+    """Pre order Traversal"""
     if(root):
         print(root.val,end=' ')
         preOrder(root.left)
         preOrder(root.right)
 
 def postOrder(root):
+    """Post order Traversal"""
     if (root):
         postOrder(root.left)
         postOrder(root.right)
         print(root.val,end=' ')
 
 def inOrder(root):
+    """In order Traversal"""
     if (root):
         inOrder(root.left)
         print(root.val,end=' ')
@@ -65,7 +64,6 @@ postOrder(root)
 # 2 3 4 5 7
 # PostOrder Traversal of the given tree:
 # 2 4 3 7 5
-#
 # Testcase 2:
 # Output:
 # PreOrder Traversal of the given tree:
@@ -74,6 +72,5 @@ postOrder(root)
 # 4 2 5 1 3
 # PostOrder Traversal of the given tree:
 # 4 5 2 3 1
-
 # Time Complexity=O(n)
 # Space Complexity=If we don’t consider size of stack for function calls then O(1) otherwise O(n).
