e207107bc9be53ce5968dc49af500cfdc7724591
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-03-12 09:26:26 +0530

    Peak Value in Array issue solved

---
+++ Array/peakvalueinarray.java
@@ -0,0 +1,50 @@
+/*
+Here we will Assume Peak element in Array is array[0] i.e 1st value,
+then we will compare it with next one, if found grater than peak then
+we will change peak to that value
+*/
+
+package peakval;
+import java.util.Scanner;
+
+public class peakvalueinarray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Size of Arraay");
+        int n = scan.nextInt();
+        if (n>0) {
+            System.out.println("Enter " + n + " Elements of array");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+            int ans = peakval(arr);
+            System.out.println();
+            System.out.println("Peak Value in Array is : " + ans);
+        }else {
+            System.out.println("Enter Valid Length of Array");
+        }
+    }
+    public static int peakval(int[] arr){
+        int peak = arr[0];
+        for (int i = 0;i<arr.length;i++){
+            if (peak<arr[i]){
+                peak = arr[i];
+            }
+        }
+        return peak;
+    }
+}
+/*
+    Test Cases:
+        Input: 5 8 9 5 2 4
+        Output: 9
+
+        Input: 3 0 1 0
+        Output: 1
+
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+
+
+ */
