9bad17b4400ed5d239e572fd38b1b660efff07aa
arnab031 <51916075+arnab031@users.noreply.github.com>
2021-03-12 19:52:29 +0530

    Update Kahn's Algorithm.py
    Third Commit

--- Code/Python/Kahn's Algorithm.py
+++ Code/Python/Kahn's Algorithm.py
@@ -7,66 +7,49 @@
 # 4: Repeat these 3 steps until the queue is empty.
 # 5: If count of visited nodes is not equal to the number of nodes in the graph then the topological sort is not possible for the given graph, else print
 #    the nodes in topologiocal sorted manner.
-
-
 from collections import defaultdict
-
-#Represent a class of Graph
 class Graph:
     def __init__(self,vertices):
-        '''Initialize vertices and dictionary containing adjacency list'''
+        """Initialize vertices and dictionary containing adjacency list"""
         self.v=vertices
-        self.graph=defaultdict(list)    
-
-    #Function to add an edge in graph
+        self.graph=defaultdict(list)
     def addEdge(self,u,v):
+        """Function to add an edge in graph"""
         self.graph[u].append(v)
-
-    #Function for topological sort
     def topological_sort(self):
+        """Function for topological sort"""
         indegree=[0]*self.v
-
         for i in self.graph:
             for j in self.graph[i]:
                 indegree[j]+=1
-
         queue=[]
         for i in range(self.v):
             if(indegree[i]==0):
                 queue.append(i)
-
         count=0
         top_order=[]
         while(queue):
             pop_ele=queue.pop(0)
             top_order.append(pop_ele)
-
             for i in self.graph[pop_ele]:
                 indegree[i]-=1
-
                 if(indegree[i]==0):
                     queue.append(i)
-
             count+=1
-
         if(count==self.v):
             print("Topological sort : ",end='')
             print(top_order)
         else:
             print("Topological Sort is not possible in this graph because there exist a cycle")
 
-
 n=int(input("Enter no of vertices : "))
 g=Graph(n)
-# m=int(input("Enter no of path: "))
 for i in range(n):
     u,v=input("Enter source and destination of a path: ").split()
     g.addEdge(int(u),int(v))
 g.topological_sort()
 
-
 # Test cases:
-
 # 1. Input:->
 # Enter no of vertices : 6
 # Enter source and destination of a path: 5 2
@@ -77,7 +60,6 @@ g.topological_sort()
 # Enter source and destination of a path: 3 1
 # Output:->
 # Topological sort : [4, 5, 2, 0, 3, 1]
-
 # 2. Input:->
 # Enter no of vertices : 6
 # Enter source and destination of a path: 5 5
@@ -88,7 +70,5 @@ g.topological_sort()
 # Enter source and destination of a path: 4 0
 # Output:->
 # Topological Sort is not possible in this graph because there exist a cycle
-
 # Time complexity: O(V+E) //Order of Sum of number of edges and vertices
 # Auxillary Space: O(V)   //Order of number of vertices
-
