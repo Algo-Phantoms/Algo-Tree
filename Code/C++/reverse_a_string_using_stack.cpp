c73666fd53873d6a97b10686e629138b086d4160
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-03-14 19:51:10 +0530

    Update reverse_a_string_using_stack.cpp

--- Code/C++/reverse_a_string_using_stack.cpp
+++ Code/C++/reverse_a_string_using_stack.cpp
@@ -1,13 +1,9 @@
 /* 
 Given a string,print reverse of the string using stack
-
 Approach:-
-
-	In stack,we know LIFO principle works.
-	
-	First,i create an empty stack,then i push each character of string in stack
-	then,using for loop i popped out every single character.
-
+In stack,we know LIFO principle works.
+First,i create an empty stack,then i push each character of string in stack
+then,using for loop i popped out every single character.
 */
 
 #include<iostream>
@@ -15,18 +11,18 @@ Approach:-
 #include<stack>
 using namespace std;
 
-# function to reverse the string
+//function to reverse the string
 void reverse(string &s)
 {
 	int i;
-	# Creating empty stack
+	//Creating empty stack
 	stack<int> str;
-	# pushing each character of the string into the stack
+	//pushing each character of the string into the stack
 	for(char ch:s)
 	{
         str.push(ch);
     	}
-	# popping each character one by one from the stack
+	//popping each character one by one from the stack
     	for(i=0;i<s.size();i++)
     	{
         s[i]=str.top();
@@ -34,31 +30,28 @@ void reverse(string &s)
     	} 
 }
 
-# driver code
+//driver code
 int main()
 {
 	cout<<"Program to reverse a string."<<endl;
 	string s;
 	cout<<"Enter a string you want to reverse:";
 	cin>>s;
-	# calling function
+	//calling function
 	reverse(s);
-	# printing result
+	//printing result
 	cout<<s;
 	return 0;
 }
 
 /*
-	Test Case :
-	
-	1.
-	  Input : Rahul
-	  Output : luhaR
-	
-	2.
-	  Input : Sajal
-	  Output : lajaS
-	
-	Time Complexity : O(n), where n is the length of the input string.
-	Space Complexity : O(n) for stack.
+Test Case :
+1.
+Input : Rahul
+Output : luhaR
+2.
+Input : Sajal
+Output : lajaS
+Time Complexity : O(n), where n is the length of the input string.
+Space Complexity : O(n) for stack.
 */
