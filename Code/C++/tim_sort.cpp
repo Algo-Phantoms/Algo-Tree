f8f383bdb5ebbfb57681969cc397fe1482a2b9e1
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-03-17 21:52:32 +0530

    Update tim_sort.cpp

--- Code/C++/tim_sort.cpp
+++ Code/C++/tim_sort.cpp
@@ -2,43 +2,42 @@
 Tim Sort is a stable sorting algorithm that combines the capabilities of  Merge sort and Insertion sort.
 It first splits the array into  small segments and sort them using Insertion Sort, and then merges the small segments using merge operation.
 It is used in Java’s Arrays.sort() as well as Python’s sorted() and sort()
-*/ 
-#include<bits/stdc++.h> 
-using namespace std; 
-const int SIZE = 16; 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const int SIZE = 16;
 
 // Perform insertion sort
-void insertion_sort(int arr[], int beg, int end) 
-{ 
-    for (int i = beg + 1; i <= end; i++) 
-    { 
-        int temp = arr[i], j = i - 1; 
-        
+void insertion_sort(int arr[], int start, int end)
+{
+    for (int i = start + 1; i <= end; i++)
+    {
+        int temp = arr[i], j = i - 1;
+
         /* Shift elements of arr[0..i-1], that are 
         greater than temp, to one position ahead 
         of their current position */
-        while (j >= beg && arr[j] > temp) 
-        { 
-            arr[j+1] = arr[j]; 
-            j--; 
-        } 
-        arr[j+1] = temp; 
-    } 
-} 
-
+        while (j >= start && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
 
 // Merge operation to merge two sorted segments
-void merge(int arr[], int beg, int mid, int end)
+void merge(int arr[], int start, int mid, int end)
 {
-    int n = mid - beg + 1, m = end - mid;
+    int n = mid - start + 1, m = end - mid;
     int arr1[n], arr2[m];
-    
+
     for (int i = 0; i < n; i++)
-        arr1[i] = arr[beg + i];
+        arr1[i] = arr[start + i];
     for (int i = 0; i < m; i++)
         arr2[i] = arr[mid + 1 + i];
 
-    int i = 0, j=0, k = beg;
+    int i = 0, j = 0, k = start;
 
     while (i < n && j < m)
     {
@@ -69,27 +68,26 @@ void merge(int arr[], int beg, int mid, int end)
 }
 
 // Sort the array using Tim Sort
-void tim_sort(int arr[], int n) 
-{ 
-    // Sort individual subarrays of size SIZE 
-    for (int i = 0; i < n; i+=SIZE) 
-        insertion_sort(arr, i, min((i+15), (n-1))); 
-    
-    // Merge all the small segments formed
-    for (int size = SIZE; size < n; size = 2*size) 
-    { 
-    // From the start of the array, we are going to merge two segments each of size SIZE.
-        for (int left = 0; left < n; left += 2*size) 
-        { 
-            // Find  mid point
-            int mid = left + size - 1; 
-            int right = min((left + 2*size - 1), (n-1)); 
-            // Merge the two subarrays
-            merge(arr, left, mid, right); 
-        } 
-    } 
-} 
+void tim_sort(int arr[], int n)
+{
+    // Sort individual subarrays of size SIZE
+    for (int i = 0; i < n; i += SIZE)
+        insertion_sort(arr, i, min((i + 15), (n - 1)));
 
+    // Merge all the small segments formed
+    for (int size = SIZE; size < n; size = 2 * size)
+    {
+        // From the start of the array, we are going to merge two segments each of size SIZE.
+        for (int left = 0; left < n; left += 2 * size)
+        {
+            // Find  mid point
+            int mid = left + size - 1;
+            int right = min((left + 2 * size - 1), (n - 1));
+            // Merge the two subarrays
+            merge(arr, left, mid, right);
+        }
+    }
+}
 
 int main()
 {
