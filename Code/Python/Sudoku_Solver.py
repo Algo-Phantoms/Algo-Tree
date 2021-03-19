8df4b52fc2f931e00fbc848853c76538316011ed
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-03-19 23:47:04 +0530

    Update Sudoku_Solver.py

--- Code/Python/Sudoku_Solver.py
+++ Code/Python/Sudoku_Solver.py
@@ -1,88 +1,139 @@
 # Title: Sudoku Solver
-# Link: https://leetcode.com/problems/sudoku-solver/
+# Program to solve a Sudoku puzzle by filling the empty cells.A sudoku solution must satisfy all of the following rules:
 
-board = [["5","3",".",".","7",".",".",".","."],
-["6",".",".","1","9","5",".",".","."],
-[".","9","8",".",".",".",".","6","."],
-["8",".",".",".","6",".",".",".","3"],
-["4",".",".","8",".","3",".",".","1"],
-["7",".",".",".","2",".",".",".","6"],
-[".","6",".",".",".",".","2","8","."],
-[".",".",".","4","1","9",".",".","5"],
-[".",".",".",".","8",".",".","7","9"]]
+# Each of the digits 1-9 must occur exactly once in each row.
+# Each of the digits 1-9 must occur exactly once in each column.
+# Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+# The '0' character indicates empty cells.
 
-import collections
-class Solution:
-    def solveSudoku(self, board: list(list())) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        def is_valid(r, c, n):
-            if n in self.rows[r] or n in self.columns[c] or n in self.sub_boxes[(r//3,c//3)]:
-                return False
-            return True
+def solve(bo):
+    """The algorith that's gonna backtrack"""
+    find = find_empty(bo)
+    
+    if not find:
+        return True
+    else:
+        row, col = find
+    
+    for i in range(1, 10):
+        if is_valid(bo, i, (row, col)):
+            bo[row][col] = i
 
-        def place_num(r, c, n):
-            self.rows[r].add(n)
-            self.columns[c].add(n)
-            self.sub_boxes[(r//3,c//3)].add(n)
-            board[r][c] = n
-
-        def remove_num(r, c, n):
-            self.rows[r].remove(n)
-            self.columns[c].remove(n)
-            self.sub_boxes[(r//3,c//3)].remove(n)
-            board[r][c] = "."
-
-        def backtrack(emp_key, emp_indice):
-            row = emp_key
-            col = self.emp[emp_key][emp_indice]
-            # based on the dict self.emp key value and the indice value, we retrieve the column value.
-            resolved = False
-            for num in range(1, 10):
-                # iterate through numbers 1-9 at the current cell.
-                if is_valid(row, col, str(num)):
-                    # examining if the num meets the rules 
-                    place_num(row, col, str(num))
-                    # fill the cell in the 2-D array and update the tracking dicts
-                    if row + 1 == 9 and emp_indice + 1 == len(self.emp[emp_key]):
-                        # we reach the bottom row and the last column elememt in the list, i.e. we find a solution!
-                        resolved = True
-                        return resolved
-                    elif emp_indice + 1 < len(self.emp[emp_key]):
-                        # we move on to the next column in the same row
-                        resolved = backtrack(emp_key, emp_indice+1)
-                    elif emp_indice + 1 == len(self.emp[emp_key]):
-                        # we move on to the next row with the first empty cell
-                        resolved = backtrack(emp_key+1, 0)
-                    if not resolved:
-                        # backtrack, i.e. remove the num from the 2-D array and from the tracking dicts
-                        remove_num(row, col, str(num))
-                    else:
-                        break
-            return resolved
+            if solve(bo):
+                return True
+            
+            bo[row][col] = 0
         
-        self.rows = collections.defaultdict(set)
-        # using dict self.rows to track the digits in each row
-        self.columns = collections.defaultdict(set)
-        # using dict self.columns to track the digits in each column
-        self.sub_boxes = collections.defaultdict(set)
-        # using dict self.sub_boxes to track the digits in each 3x3 sub-box
-        self.emp = collections.defaultdict(list)
-        # using dict self.emp to track the empty cells in the 2-D array
-        # Note that for dict self.emp, the key is the 2-D array row number, and the value is the 2-D array column numbers.
-        # E.g., 0:[2, 3, 5, 6, 7, 8] --> row number is 0, column numbers are 2, 3, 5, 6, 7, and 8
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != ".":
-                    self.rows[i].add(board[i][j])
-                    self.columns[j].add(board[i][j])
-                    self.sub_boxes[(i//3,j//3)].add(board[i][j])
-                else:
-                    self.emp[i].append(j)
-        # we work on dict self.emp, pass in the first row number 0, and the first indice value of the dict value list
-        # which includes the column number values.
-        backtrack(0, 0)
+    return False
 
-solution = Solution()
-solution.solveSudoku(board)
+
+
+def is_valid(bo, num, pos):
+    """Returns bollean expresion whether inserting some number
+    in an empty space is valid or not"""
+
+    #Check whether the number is already in the current row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    #Check whether the number is already in the current column 
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    #Check whether the number is already in the current box
+    box_X_coo = pos[1] // 3 #Will be an integer from 0-2
+    box_Y_coo = pos[0] // 3
+
+    for i in range(box_Y_coo * 3, box_Y_coo * 3 + 3):
+        for j in range(box_X_coo * 3, box_X_coo * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print('- - - - - - - - - -')
+        
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print('|', end = "")
+            
+            if j == 8:
+                print(str(bo[i][j])) #When it prints the 9th element of a row, inserts a \n
+            else:                    #Otherwhise, remains in the same line
+                print(str(bo[i][j]) + " ", end = "")
+            
+def find_empty(bo):
+    """Returns a tuple (row, col) with the coordinates of an 
+    empty space on the board"""
+
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)
+    return None
+
+
+solve(board)
+print_board(board)
+
+#Test Cases
+      
+# 1st Case
+# board = [
+#     [7,8,0,4,0,0,1,2,0],
+#     [6,0,0,0,7,5,0,0,9],
+#     [0,0,0,6,0,1,0,7,8],
+#     [0,0,7,0,4,0,2,6,0],
+#     [0,0,1,0,5,0,9,3,0],
+#     [9,0,4,0,6,0,0,0,5],
+#     [0,7,0,3,0,0,0,1,2],
+#     [1,2,0,0,0,7,4,0,0],
+#     [0,4,9,2,0,6,0,0,7]
+# ]
+
+# Output:
+# 7 8 5 |4 3 9 |1 2 6
+# 6 1 2 |8 7 5 |3 4 9
+# 4 9 3 |6 2 1 |5 7 8
+# - - - - - - - - - -
+# 8 5 7 |9 4 3 |2 6 1
+# 2 6 1 |7 5 8 |9 3 4
+# 9 3 4 |1 6 2 |7 8 5
+# - - - - - - - - - -
+# 5 7 8 |3 9 4 |6 1 2
+# 1 2 6 |5 8 7 |4 9 3
+# 3 4 9 |2 1 6 |8 5 7
+      
+      
+#2nd case
+   
+# board =[[5, 3, 0, 0, 7, 0, 0, 0, 0],
+# [6, 0, 0, 1, 9, 5, 0, 0, 0],
+# [0, 9, 8, 0, 0, 0, 0, 6, 0],
+# [8, 0, 0, 0, 6, 0, 0, 0, 3],
+# [4, 0, 0, 8, 0, 3, 0, 0, 1],
+# [7, 0, 0, 0, 2, 0, 0, 0, 6],
+# [0, 6, 0, 0, 0, 0, 2, 8, 0],
+# [0, 0, 0, 0, 1, 9, 0, 0, 5],
+# [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+# Output:
+# 5 3 4 |6 7 8 |1 9 2
+# 6 7 2 |1 9 5 |3 4 8
+# 1 9 8 |3 4 2 |5 6 7
+# - - - - - - - - - -
+# 8 5 9 |7 6 1 |4 2 3
+# 4 2 6 |8 5 3 |9 7 1
+# 7 1 3 |9 2 4 |8 5 6
+# - - - - - - - - - -
+# 9 6 1 |5 3 7 |2 8 4
+# 2 8 7 |4 1 9 |6 3 5
+# 3 4 5 |2 8 6 |7 1 9
+
+
+#Time complexity: O(9^(n*n))
+# Space Complexity: O(n*n)
+
