e4040e9f70d6df34b81f6d062259dbe21b136209
Chitra Singla <45913363+chitrasingla@users.noreply.github.com>
2021-03-19 11:31:41 +0530

    Update repeating_and_missing_number.cpp

--- Code/C++/repeating_and_missing_number.cpp
+++ Code/C++/repeating_and_missing_number.cpp
@@ -24,28 +24,28 @@ int main()
 
     // taking array as input
     int a[n];
-    for(int i=0;i<n;i++)
+    for(int i = 0 ; i < n ; i++)
         cin>>a[i];
 
-    int i=0;
-    while(i<n)
+    int i = 0;
+    while(i < n)
     {
         // if element is at its position
-        if(a[i]==i+1 || a[i]==a[a[i]-1])
+        if(a[i] == i + 1 || a[i] == a[a[i] - 1])
             i++;
         else
             // if not swap
-            swap(a[i],a[a[i]-1]);
+            swap(a[i],a[a[i] - 1]);
     }
 
-    int r=0,m=0;
-    for(int i=0;i<n;i++)
+    int r = 0, m = 0;
+    for(int i = 0 ; i < n ; i++)
     {
         // find the element which is not at its position
-        if(a[i]!=i+1)
+        if(a[i] != i + 1)
         {
             r = a[i];
-            m = i+1;
+            m = i + 1;
             break;
         }
     }
@@ -54,19 +54,21 @@ int main()
     cout<<"Missing number is "<<m;
 }
 /*
-	Testcases
-	Input :
-	5
-	4 1 3 2 1
-	Output : Repeating number is 1
-	Missing number is 5
+Testcases
+Input :
+5
+4 1 3 2 1
+Output : Repeating number is 1
+Missing number is 5
 
-	Input :
-	6
-	1 4 5 2 3 4
-	Output : Repeating number is 4
-	Missing number is 6
+Input :
+6
+1 4 5 2 3 4
+Output : Repeating number is 4
+Missing number is 6
 
-	Time complexity : O(n)
-	Space complexity : O(1)
+Time complexity : O(n)		
+Space complexity : O(1)
+
+where n is the size of the array
 */
