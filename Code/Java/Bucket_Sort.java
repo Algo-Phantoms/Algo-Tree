7189bb4f1d40a00d80feb750fdbb03e4c777a395
Muthu Annamalai Venkatachalam <muthuannamalai2002@gmail.com>
2021-03-20 11:25:50 +0530

    Update Bucket_Sort.java

--- Code/Java/Bucket_Sort.java
+++ Code/Java/Bucket_Sort.java
@@ -30,20 +30,20 @@ class BucketSort {
         }
 
         //dividing numbers in different buckets
-        for (int i=0; i<length; i++) {
+        for (int i = 0; i < length; i++) {
             int bucketIndex = (int) ((arr[i] * k) / max_val);
             bucketList.get(bucketIndex).add(arr[i]);
         }
 
         // sorting each bucket one by one
-        for (int i=0; i<length; i++) {
+        for (int i = 0; i < length; i++) {
             Collections.sort(bucketList.get(i));
         }
 
         //joining buckets
         int pos = 0;
-        for (int i=0; i<k; i++) {
-            for (int j=0; j<bucketList.get(i).size(); j++) {
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j<bucketList.get(i).size(); j++) {
                 arr[pos] = bucketList.get(i).get(j);
                 pos += 1;
             }
@@ -58,7 +58,7 @@ class BucketSort {
         int size = sc.nextInt();
         float arr[] = new float[size];
         System.out.println("Enter array elements:");
-        for (int i=0; i<size; i++) {
+        for (int i = 0; i < size; i++) {
             arr[i] = sc.nextFloat();
         }
         System.out.println("Enter number of buckets:");
@@ -66,7 +66,7 @@ class BucketSort {
 
         // before sorting
         System.out.println("Array before bucket sort:");
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
@@ -75,7 +75,7 @@ class BucketSort {
 
         // after sorting
         System.out.println("Array after Bucket sort:");
-        for (int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         sc.close();
@@ -84,7 +84,7 @@ class BucketSort {
 
 /*
 Test Cases:
-Input:
+Example 1:
 Enter size of array:
 10
 Enter array elements:
@@ -107,7 +107,27 @@ Array before bucket sort:
 Array after Bucket sort:
 11.0 26.0 33.0 49.0 62.0 77.0 85.0 88.0 91.0 99.0
 
-Time Complexity:  O(n^2)
-Space Complexity: O(nk)
+Example 2:
+Enter size of array:
+7
+Enter array elements:
+0.49
+5.9
+3.41.11
+4.5
+6.6
+2.0
+Enter number of buckets:
+3
 
+Output:
+Array before bucket sort:
+0.49 , 5.9 , 3.4 , 1.11 , 4.5 , 6.6 , 2.0
+Array after Bucket Sort:
+0.49 , 1.11 , 2.0 , 3.4 , 4.5 , 5.9 , 6.6
+
+Time Complexity:  O(n^2)
+Space Complexity - O(nk)
+where n = number of elements in the input array
+where k = number of buckets
 */
