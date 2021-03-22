0e888f2c3389326e21ebe5d16f79418831ffb626
Manav Arora <72567005+ManavArora26@users.noreply.github.com>
2021-03-22 20:40:29 +0530

    Insertion sort in python

--- Code/Python/insertionsort.py
+++ Code/Python/insertionsort.py
@@ -12,7 +12,7 @@ def insertionSort(arr):
         while j >=0 and key < arr[j] : 
                 arr[j+1] = arr[j] 
                 j -= 1
-        arr[j+1] = key 
+        arr[j+1] = key
   
   
 # Driver code to test above 
