6d39f39e76f0fe7cb3444614deb595cdbe67c4d0
malavikarajeshvikraman <42905287+malavikarajeshvikraman@users.noreply.github.com>
2021-03-25 09:47:08 +0530

    Made changes

--- Code/Python/linearsearch.py
+++ Code/Python/linearsearch.py
@@ -3,20 +3,25 @@
 Linear search, also known as sequential search, is a search algorithm which examines each element in the order it is presented to find the specified data.
  
 '''
- 
+#Linear Search Function
 def linearSearch(item,l):
     found = False
     position = 0
+   #Traversing through array and checking if element is present 
     while position < len(l) and not found:
         if l[position] == item:
             found = True
         position = position + 1
     return found
-    
+
+#Obtaining the list  from input
 l = list(map(int,input().strip().split()))
 item = int(input())
 
+#Calling the linear search fuction 
 itemFound = linearSearch(item,l)
+
+#Displaying the output
 if itemFound:
     print ('True')
 else:
@@ -38,6 +43,6 @@ Output  : False
  
 Time Complexity: O(n)
  
-Space Complexity : O(n)
+Space Complexity : O(1)
 
 '''
