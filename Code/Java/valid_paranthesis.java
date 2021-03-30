8458ad7a33c7b96ffd5d7079952fa0a7f789a5e1
Shahina <56913578+shahina-bano@users.noreply.github.com>
2021-03-31 01:05:22 +0530

    Add files via upload

---
+++ Code/Java/valid_paranthesis.java
@@ -0,0 +1,85 @@
+/*Generate all valid Paranthesis
+Problem  Statement: Write a program to generate all possible "n" pairs of balanced paranthesis.
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Paranthesis{
+        
+        //function to print all valid pairs of paranthesis by storing the count of opening and closing brackets
+        static void generate_paranthesis(char str[], int pos, int n, int start, int end){
+            if(end == n){
+                for(int i = 0; i < str.length; i++){
+                    System.out.print(str[i]);           //printing all pairs of paranthesis
+                }
+                
+                System.out.println();
+                return;
+            }
+            else{
+                if(start > end){                       //checking the condition for inserting the closing paranthesis
+                    str[pos] = '}';
+                    generate_paranthesis(str, pos+1, n, start, end+1);     //calling the function for the next position using recursion
+                }
+                if(start < n){                          //checking the condition for inserting the opeing paranthesis
+                    str[pos] = '{';
+                    generate_paranthesis(str, pos+1, n, start+1, end);    //calling the function for the next position using recursion
+                }
+            }
+        }
+        
+        //function to call the generate_paranthesis function with initial values
+        static void valid_paranthesis(char ans[], int n){
+            if( n > 0)
+                generate_paranthesis(ans, 0, n, 0 , 0);
+            
+            return;
+        }
+    
+    	public static void main (String[] args) throws java.lang.Exception
+    	{
+    	        Scanner sc = new Scanner(System.in); 
+                int n = 0;
+                n = sc.nextInt();                               //input from the users
+                char [] arr = new char[2*n];                    //generating a character array for storing paranthesis
+        		valid_paranthesis(arr, n);                      //calling the function
+    	}
+}
+
+/*Test Case 1: 
+        Input: 
+                3
+        Output:
+                {}{}{}
+                {}{{}}
+                {{}}{}
+                {{}{}}
+                {{{}}}
+               
+Test Case 2: 
+        Input: 
+                 4
+        Output:
+                {}{}{}{}
+                {}{}{{}}
+                {}{{}}{}
+                {}{{}{}}
+                {}{{{}}}
+                {{}}{}{}
+                {{}}{{}}
+                {{}{}}{}
+                {{}{}{}}
+                {{}{{}}}
+                {{{}}}{}
+                {{{}}{}}
+                {{{}{}}}
+                {{{{}}}}
+
+               
+Time Complexity: O(2^n)
+
+Space Complexity: O(n)
+              
+*/
