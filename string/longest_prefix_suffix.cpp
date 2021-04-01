f8db811f76c1645cb57674d42658e24c0921dbdb
Ritika Verma <76901399+ansi22@users.noreply.github.com>
2021-04-02 00:32:10 +0530

    Update longest_prefix_suffix.cpp

--- string/longest_prefix_suffix.cpp
+++ string/longest_prefix_suffix.cpp
@@ -20,36 +20,35 @@
  than two, no such substring exist
  hence prints 0*/
  if(s.length()<2)
-    cout<<0;
+     cout<<0;
  else{
-    /*divide string into two equal parts
-      and store starting indices of both*/
-    int l=0,i=s.length()/2;
-    /*loop calculating the length of
+     /*divide string into two equal parts
+     and store starting indices of both*/
+     int l=0,i=s.length()/2;
+     /*loop calculating the length of
      such substring*/
      while(i<s.length())
      {
     /*check if left string character is equal
      to right one if so increament indices of both by 1*/
-        if(s[i]==s[l])
-        {
-         ++l;
-         ++i;
-         }
+     if(s[i]==s[l])
+     {
+     ++l;
+     ++i;
+     }
     /*otherwise check for shoter substring*/
-        else
-        {
-        i=i-l+1;
-        l=0;
-        }
+     else
+     {
+     i=i-l+1;
+     l=0;
+     }
      }
     /*as overlapping is not allowed*/
-    if(l>s.length()/2) 
-        cout<<s.length()/2 ;
-    else 
-        cout<<l;
-    }
-
+ if(l>s.length()/2) 
+      cout<<s.length()/2 ;
+ else 
+      cout<<l;
+}
 return 0;
 }
 
