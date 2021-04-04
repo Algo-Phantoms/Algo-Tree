6b31734351927a2eed1b1f54a118ed997baf9f6a
khusheekapoor <74901388+khusheekapoor@users.noreply.github.com>
2021-04-04 10:56:58 +0530

    Update Tower_of_Hanoi.java

--- Code/Java/Tower_of_Hanoi.java
+++ Code/Java/Tower_of_Hanoi.java
@@ -1,26 +1,21 @@
-
-/**
- * Tower of Hanoi. Recursive method is used.
- * Author - Khushee Kapoor
- * Shift(N,A,B,C)
- * A,B,C are 3 pegs such that peg A contains N number of discs. The discs are to be moved from peg A to 
- * peg C in ascending order of their sizes. The method used for this purposive is recursive method.
- * Algorithm used:
- * 1. [Set the base case]
- *      If(N==1)
- *      Display("Moving Disc A -> C")
- *      else
- * 2. [Recurisve Case]
- *      Shift(N-1,A,C,B)
- *      Display("Moving Disc A -> C")
- *      Shift(N-1,B,A,C)
- *      
- * So first recursive call moves n-1 disks from ‘first’ to ‘middle’ using ‘last’. So after that call 
- * n-1 disks are in ‘middle ‘ peg in order of size and the ‘first’ peg contains the nth disk i.e, 
- * the largest one. So, now move that disk from ‘first’ peg to ‘last’ peg. Then again by the 
- * 2nd recursive call move n-1 disk from ‘middle’ peg to ‘last’ peg using ‘first’ peg. So , after 
- * all these, ‘last’ peg contains all disks in order of size . 
- */
+/* Tower of Hanoi. Recursive method is used.
+  Shift(N,A,B,C)
+  A,B,C are 3 pegs such that peg A contains N number of discs. The discs are to be moved from peg A to 
+  peg C in ascending order of their sizes. The method used for this purposive is recursive method.
+  Algorithm used:
+  1. [Set the base case]
+       If(N==1)
+       Display("Moving Disc A -> C")
+       else
+  2. [Recurisve Case]
+       Shift(N-1,A,C,B)
+       Display("Moving Disc A -> C")
+       Shift(N-1,B,A,C)
+  So first recursive call moves n-1 disks from ‘first’ to ‘middle’ using ‘last’. So after that call 
+  n-1 disks are in ‘middle ‘ peg in order of size and the ‘first’ peg contains the nth disk i.e, 
+  the largest one. So, now move that disk from ‘first’ peg to ‘last’ peg. Then again by the 
+  2nd recursive call move n-1 disk from ‘middle’ peg to ‘last’ peg using ‘first’ peg. So , after 
+  all these, ‘last’ peg contains all disks in order of size . */
 import java.util.*;
 public class Tower_of_Hanoi
 {
@@ -47,37 +42,35 @@ public class Tower_of_Hanoi
        ob.Shift(N,"A","B","C");
     }
 }
-/** Sample Input: 3
- * Sample Output:
- * Disc moved from 	A->C
- * Disc moved from 	A->B
- * Disc moved from 	C->B
- * Disc moved from 	A->C
- * Disc moved from 	B->A
- * Disc moved from 	B->C
- * Disc moved from 	A->C
-*/
+/* Test Cases-
+  Input: 3
+  Output:
+  Disc moved from 	A->C
+  Disc moved from 	A->B
+  Disc moved from 	C->B
+  Disc moved from 	A->C
+  Disc moved from 	B->A
+  Disc moved from 	B->C
+  Disc moved from 	A->C
+  Input: 4
+  Output:
+  Disc moved from 	A->B
+  Disc moved from 	A->C
+  Disc moved from 	B->C
+  Disc moved from 	A->B
+  Disc moved from 	C->A
+  Disc moved from 	C->B
+  Disc moved from 	A->B
+  Disc moved from 	A->C
+  Disc moved from 	B->C
+  Disc moved from 	B->A
+  Disc moved from 	C->A
+  Disc moved from 	B->C
+  Disc moved from 	A->B
+  Disc moved from 	A->C
+  Disc moved from 	B->C */
 
-/** Sample Input: 4
- * Sample Output:
- * Disc moved from 	A->B
- * Disc moved from 	A->C
- * Disc moved from 	B->C
- * Disc moved from 	A->B
- * Disc moved from 	C->A
- * Disc moved from 	C->B
- * Disc moved from 	A->B
- * Disc moved from 	A->C
- * Disc moved from 	B->C
- * Disc moved from 	B->A
- * Disc moved from 	C->A
- * Disc moved from 	B->C
- * Disc moved from 	A->B
- * Disc moved from 	A->C
- * Disc moved from 	B->C
-*/
-
-/** Time Complexity: O(2^n)
- * Space Complexity: O(n) */
+/* Time Complexity: O(2^n)
+   Space Complexity: O(n) */
  
  
