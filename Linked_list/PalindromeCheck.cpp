26ef7e2f2eab0bbf090cea1f9f5b22ee72d47fa8
AnushkaS09 <67291868+AnushkaS09@users.noreply.github.com>
2021-04-09 01:59:53 +0530

    Create PalindromeCheck.cpp

---
+++ Linked_list/PalindromeCheck.cpp
@@ -0,0 +1,55 @@
+/*Check if the linked list is palindrome or not if yes return true if not return false. Linked list has to be taken as an input from the user.*/
+Node* reverse(Node *save)
+{
+   if(save==NULL || save->next==NULL)  
+    {
+
+        return save;
+    }
+    
+    Node *new_head = reverse(save->next);
+    Node *temp = save->next;      
+    temp->next = save;
+    save->next = NULL;
+    return new_head; 
+}
+bool isPalindrome(Node *head)
+{
+    Node *help=head;
+  Node *temp = reverse(help);
+    
+    if(head==NULL){
+        return 1;
+    }
+    while(temp && help){
+        if(temp->data != help->data){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+        temp = temp->next;
+        head = head->next;
+    }
+}
+/*
+----------------------------------------------------------------------------------
+SAMPLE TEST CASE 1:
+INPUT:
+Enter number of test cases:1
+enter the linked list(enter -1 at the end of the linked list):3 9 7 9 3 -1
+OUTPUT:
+true
+-------------------------------------------------------------------------------------
+SAMPLE TEST CASE 2:
+
+ Enter number of test cases:2
+ enter the linked list(enter -1 at the end of the linked list):3 1 3 -1
+ true
+ 1 2 3 -1
+ false
+
+*/
+
+
+
