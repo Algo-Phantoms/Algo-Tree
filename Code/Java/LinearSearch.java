c966332046e80e662e791e9bfb7db30949eb4ca5
GarimaKhare15 <garimakhare.cse19@chitkarauniversity.edu.in>
2021-04-08 11:11:56 +0530

    Added code for linear search in Java

--- Code/Java/LinearSearch.java
+++ Code/Java/LinearSearch.java
@@ -1,4 +1,8 @@
- package array;
+/*
+Linear Search : It is an algorithm which is used to search an element in the given array.
+It returns -1 if the element is not present in the array else it will return the index where that element is present in the array
+*/
+package array;
 import java.util.Scanner;
 public class LinearSearch {
 
@@ -39,11 +43,14 @@ public class LinearSearch {
 /* TEST CASE 1
  * Input:
  * 5 (size of array)
- * 1 2 3 4 5(elements of an array)
+ * 5 7 3 4 6(elements of an array)
  * 3(element which has to be searched in the given array)
  * 
  * Output:
  * 3 is present at index 2 
+ * (output is 2 because index starts from zero)
+ * 
+ * xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
  * 
  * TEST CASE 2
  * Input:
@@ -51,5 +58,12 @@ public class LinearSearch {
  * 1 2 5 6 34 78 23 90 4 29
  * 30
  * 
+ * Output:
+ * 30 is not present in the given array
 
+
+ */
+
+/*SPACE COMPLEXITY : O(1)
+ * TIME COMPLEXITY : O(N)
  */
