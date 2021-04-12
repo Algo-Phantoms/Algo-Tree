e34b404d633c19e0fac4875b5ae25f8db1cecd6d
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-04-12 23:40:39 +0530

    Update unrolled_linked_list.cpp

--- Code/C++/unrolled_linked_list.cpp
+++ Code/C++/unrolled_linked_list.cpp
@@ -93,10 +93,12 @@ int main()
     Time complexity : O(n)
 
     Test Case 1-
+    Input: 1  2  3  4  5  6  7  8  9  10  11  12
     Output:
     1  2  3  4  5  6  7  8  9  10  11  12
 
     Test Case 2-
+    Input: 10 20 30 40 50 60 70 80 90 100 110 120
     Output:
     10 20 30 40 50 60 70 80 90 100 110 120
 */
