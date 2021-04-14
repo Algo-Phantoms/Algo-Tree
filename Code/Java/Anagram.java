4333d4a3e98853c8479979d8b6e9a51aeaff39e6
PRATEEK NAIR <56732496+prateek-jpg@users.noreply.github.com>
2021-04-14 09:37:15 +0530

    Update Anagram.java

--- Code/Java/Anagram.java
+++ Code/Java/Anagram.java
@@ -1,5 +1,5 @@
-
-/*Two strings are said to be an Anagram if all the characters of a string are equal and have same frequency but arranged in different order.
+/*
+Two strings are said to be an Anagram if all the characters of a string are equal and have same frequency but arranged in different order.
 The simple logic to solve is taking the two strings into a character array and sort both the arrays and again store it to the same string variable,
 if both the strings are equal then one string is said to be anagram of another.
 */
@@ -10,30 +10,32 @@ public class Anagram {
 
 	public static void main(String[] args) {
 	
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter string 1:");
-		String s=sc.next();
-		System.out.println("Enter string 2:");
-		String s1=sc.next();
-		        
-		int l1=s.length(),l2=s1.length(); // storing the length of both the strings
+		System.out.println("Enter string 1: ");
+		String s = sc.next();
+		System.out.println("Enter string 2: ");
+		String s1 = sc.next();
+		 // storing the length of both the strings       
+		int l1 = s.length(),l2 = s1.length(); 
 		
 		if(l1!=l2) {
-			System.out.println("OUTPUT:Two strings are not Anagram"); //if both are of unequal length then its for sure that they are not an anagram
+			//if both are of unequal length then its for sure that they are not an anagram
+			System.out.println("OUTPUT:Two strings are not Anagram"); 
 		}
 		else {
-			char c1[]=s.toCharArray(),c2[]=s1.toCharArray();
+			char c1[] = s.toCharArray(),c2[] = s1.toCharArray();
 			
-			s="";s1="";
-			Arrays.sort(c1);Arrays.sort(c2);//sorting both the array.
+			s = ""; s1 = "";
+			//sorting both the array.
+			Arrays.sort(c1);Arrays.sort(c2);
 			
 			for(char ch:c1) {
-				s=s+ch;
+				s = s + ch;
 			}
 			
 			for(char ch:c2) {
-				s1=s1+ch;
+				s1 = s1 + ch;
 			}
 			if(s.compareTo(s1)==0) {
 				System.out.println("OUTPUT:Two strings are Anagram");
@@ -45,17 +47,19 @@ public class Anagram {
 }
 
 
-/*  OUTPUT
-    1. Enter string 1-prateek
-	   Enter string 2-eektarp
-       OUTPUT-Two strings are Anagram
+/*  
+             OUTPUT
+    INPUT: Enter string 1: prateek
+	   Enter string 2: eektarp
+       OUTPUT:  Two strings are Anagram
     
-    2. Enter string 1-abcde
-       Enter string 2-cdba       
-      OUTPUT-Two strings are not Anagram
+    INPUT: Enter string 1: abcde
+           Enter string 2: cdba       
+      OUTPUT:  Two strings are not Anagram
       
       
       Time Complexity-O(nlog n)
-      Space Complexity-O(n)    
+      Space Complexity-O(n) 
+      where n: size of the input
     
     */
