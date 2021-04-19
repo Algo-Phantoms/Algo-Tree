33d941c73e6e33139ea43fba3bbf38f6c6409b45
Ritika Katoch <71933308+Ritikakatoch0128@users.noreply.github.com>
2021-04-19 16:18:37 +0530

    Update inverseArray.py

--- Code/Python/inverseArray.py
+++ Code/Python/inverseArray.py
@@ -3,6 +3,7 @@ Problem statement:   If the array elements are swapped with their correseponding
                      indices,we get the inverse of an array
 NOTE: The elements must be unique and less than the length of array.
 '''
+
 #inserts the elements in an array of size n
 def inputElements(n):
     arr=[]
@@ -12,6 +13,7 @@ def inputElements(n):
          arr.append(element)
     print("original array:",arr)
     return arr
+  
 #function to find inverse of array
 def inverseArray(lst):
     inverted_array=[]
@@ -21,19 +23,15 @@ def inverseArray(lst):
     return inverted_array
 
 #main code
-# Enter the number of testcases
-t=int(input()) 
-for i in range(0,t):
-    # enter length of array
-    array_length=int(input())
-    arr=inputElements(array_length)
-    arr2=inverseArray(arr)
-    print("Inverse of array :",arr2)
+# enter length of array
+array_length=int(input())
+arr=inputElements(array_length)
+arr2=inverseArray(arr)
+print("Inverse of array :",arr2)
 
 '''
 TestCases :
-2
-Input:
+Input :
 5
 2 4 1 0 3
 
@@ -41,11 +39,11 @@ Output:
 original array: [2,4,1,0,3]
 Inverse of array: [3,2,0,4,1]
 
-Input:
+Input :
 4
 1 3 0 2
 
-Output:
+Output :
 Original array:[1,3,0,2]
 Inverse of array:[2,0,3,1]
 
