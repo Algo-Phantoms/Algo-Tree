b91e15e8982d7e40c963a6a7c66aa080657d40b3
Yashmita <55138349+yashmita@users.noreply.github.com>
2021-04-21 16:50:13 +0530

    Update Book_Allocation.py

--- Code/Python/Book_Allocation.py
+++ Code/Python/Book_Allocation.py
@@ -1,88 +1,80 @@
 '''
 PROBLEM STATEMENT:
-Given an array of integers of size n, where n denotes number of books and each element of an array denotes the number of pages in the ith book, also given another integer 
-denoting number of students. The task is to allocate books to the given number of students so that maximum number of pages allocated to a student is minimum. A book will be 
-allocated to exactly one student and each student has to be allocated atleast one book. Allotment should be in contiguous order, for example, a student cannot be allocated 
-book 1 and book 3, skipping book 2. Calculate and return that minimum possible number. Return -1 if a valid assignment is not possible.
+Given an array of integers of size n, where n denotes number of books and each
+element of an array denotes the number of pages in the ith book, also given
+another integer denoting number of students. The task is to allocate books to
+the given number of students so that maximum number of pages allocated to a
+student is minimum. A book will be allocated to exactly one student and each
+student has to be allocated atleast one book. Allotment should be in contiguous
+order, for example, a student cannot be allocated book 1 and book 3, skipping
+book 2. Calculate and return that minimum possible number. Return -1 if a valid
+assignment is not possible.
 '''
 
 
-# Function to check whether it is possible to allocate books using the current minimum value
-def IsPossible(lis, n, stud, minval): 
-
+# Function to check whether it is possible to allocate books using the current
+# minimum value
+def IsPossible(lis, n, stud, minval):
     students = 1
     sumval = 0
-    for i in range(n): 
-  
-        # If the number of pages in a book is greater than the minimum value, then the minimum value chosen cannot be used to alloacte books 
-        if (lis[i] > minval): 
+    for i in range(n):
+        # If the number of pages in a book is greater than the minimum value,
+        # then the minimum value chosen cannot be used to allocate books
+        if (lis[i] > minval):
             return False
-  
         # Counting number of students required for allocating minval pages
         if (sumval + lis[i] > minval):
             students += 1
-            sumval = lis[i] 
-  
-            # If more number of students are required than the given number of students, then also it is not possible to allocate books
-            if (students > stud): 
+            sumval = lis[i]
+            # If more number of students are required than the given number of
+            # students, then also it is not possible to allocate books
+            if (students > stud):
                 return False
-  
-        else: 
-            sumval += lis[i] 
-  
+        else:
+            sumval += lis[i]
     return True
-  
-    
-# Function to find the maximum number of pages allocated to a student is minimum
-def MinPages(lis, n, stud): 
-  
+
+
+# Function to find maximum number of pages allocated to a student is minimum
+def MinPages(lis, n, stud):
     sum = 0
-  
-    # If number of students are more than number of books, we'll return -1 because all the students won't get a book
-    if (n < stud): 
+    # If number of students are more than number of books, we'll return -1
+    # because all the students won't get a book
+    if (n < stud):
         return -1
-    
-    for i in range(n): 
-        sum += lis[i] 
-  
+    for i in range(n):
+        sum += lis[i]
     low, high = 0, sum
-    ans = 10**9;
-    
+    ans = 10**9
     while (low <= high):
         mid = (low + high) // 2
-
-        if (IsPossible(lis, n, stud, mid)): 
-  
-            # If possible, then comparing the mid value with the answer that we have so far
-            ans = min(ans, mid)   
-            # Because the number of pages are given in ascending order and we are looking for the minimum value possible, so for the next possible answer, we will reduce our search by decreasing high to mid-1
+        if (IsPossible(lis, n, stud, mid)):
+            # If possible, then comparing the mid value with the answer that we
+            # have so far
+            ans = min(ans, mid)
             high = mid - 1
-  
-        else: 
-            # If it is not possible to allocate books using mid value as the minimum value, then we will look for the minimum element in the other half that is low will be increased to mid + 1
+        else:
             low = mid + 1
     return ans
 
 
 # Driver Code
-n = int(input("Enter the size of an array: ")) 
+n = int(input("Enter the size of an array: "))
 lis = []
-print("Enter ", n ," elements:")
+print("Enter ", n, " elements:")
 # User inputs number of pages in each book in ascending order
 for i in range(n):
     num = int(input())
     lis.append(num)
 stud = int(input("Enter the number of students: "))
-  
 # A function call to MinPages
-print("Minimum number of pages are: ",  MinPages(lis, n, stud)) 
+print("Minimum number of pages are: ",  MinPages(lis, n, stud))
 
 
 '''
 TEST CASES:
-
 1.
-Input: 
+Input:
 Enter the size of an array: 4
 Enter 4 elements:
 12
@@ -95,14 +87,14 @@ Output:
 Minimum number of pages are: 113
 
 Explanation:
-There are 2 students. Books can be allocated in the following way: 
+There are 2 students. Books can be allocated in the following way:
 1) [12] and [34, 67, 90]
 student 1 gets 12 pages
 student 2 gets 34 + 67 + 90 = 191 pages
 Maximum of the two = 191
 2) [12, 34] and [67, 90]
 student 1 gets 12 + 34 = 36 pages
-student 2 gets 67 + 90 = 157 pages 
+student 2 gets 67 + 90 = 157 pages
 Maximum of the two = 157 pages
 3) [12, 34, 67] and [90]
 student 1 gets 12 + 34 + 67 = 113 pages
@@ -112,7 +104,7 @@ Maximum of the two = 113 pages
 Out of the 3 cases, Option 3 has the minimum pages = 113.
 
 2.
-Input: 
+Input:
 Enter the size of an array: 5
 Enter 5 elements:
 34
@@ -126,8 +118,8 @@ Output:
 Minimum number of pages are: -1
 
 Explanation:
-Because number of students are more than number of books. Therefore, there is no valid
-assignment possible.
+Because number of students are more than number of books. Therefore,
+there is no valid assignment possible.
 
 TIME COMPLEXITY: O(N*LOG(Sum of pages)), where 'N' denotes number of books.
 SPACE COMPLEXITY: O(1), no extra space used.
