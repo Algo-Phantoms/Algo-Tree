9a0fd96232450ab75171b6ef61701663aa92d2eb
Sayantani Saha <75435974+sayantani11@users.noreply.github.com>
2021-04-21 21:33:54 +0530

    Update Tower_of_Hanoi.py

--- Code/Python/Tower_of_Hanoi.py
+++ Code/Python/Tower_of_Hanoi.py
@@ -15,38 +15,56 @@ The task is to take all those disks from one rod to some other rod following the
 
  Program will print the source and destination to transfer each disks for all the steps in chronological order.'''
 
-
+# This function takes the number of disks and the names of the rods.
+#	The idea is to shift one disk from the source to the destination but
+#	if there is already mutiple disk on the source then call the same function to put the extra disk on the helper rod and
+#	if there is already mutiple disk on the destination then call the same function to put the extra disk on the helper rod. 
+ 
 def Tower_of_Hanoi(n,src,dest,help):
 
-   ''' This function takes the number of disks and the names of the rods.
-	The Idea is to shift one disk from the source to the destination.
-	BUT,
-	if there is already mutiple disk on the source then call the same function to put the extra disk on the helper rod.
-	AND,
-	if there is already mutiple disk on the destination then call the same function to put the extra disk on the helper rod.'''  
-
-    if(n==0):
+    if(n==0): # defining the base case of the function
         return
 
-    Tower_of_Hanoi(n-1,src,help,dest)
+    Tower_of_Hanoi(n-1,src,help,dest) # recursively calling the function
+    # moving disk from source to helper rod
+
     print(f"Move from {src} to {dest}")
-    Tower_of_Hanoi(n-1,help,dest,src)
 
+    Tower_of_Hanoi(n-1,help,dest,src) #moving disk from helper to destination rod
 
+#Driver code of the function
 if __name__=='__main__':
 
-    n=int(input("Enter number of rods: "))
+    n=int(input("Enter number of rods: "))  # taking the number of rods from the user
 
     Tower_of_Hanoi(n,'A','B','C')
+    # A B C are the name of rods
+
+''' 
+TESTCASES:
+
+Sample Input :
+   Enter number of rods: 3
+
+Sample Output:
+  Move from A to B
+  Move from A to C
+  Move from B to C
+  Move from A to B
+  Move from C to A
+  Move from C to B
+  Move from A to B 
 
 
-''' Sample Input = 3
 
- Sample Output:
-     Move from A to B
-     Move from A to C
-     Move from B to C
-     Move from A to B
-     Move from C to A
-     Move from C to B
-     Move from A to B '''
+Sample Input:
+   Enter number of rods: 2
+
+Sample Output:
+   Move from A to C
+   Move from A to B
+   Move from A to C
+
+   Time Complexity : O(2^n)
+   Space Complexity : O(n)
+'''
