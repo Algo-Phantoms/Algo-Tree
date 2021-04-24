54ee6dc7411cedbe6467c9e560219b5e35f2d7e9
Shalvi Singhal <shalvisinghal01@gmail.com>
2021-04-24 12:39:05 +0530

    changes made

--- Code/Python/trapping_rainwater.py
+++ Code/Python/trapping_rainwater.py
@@ -1,13 +1,13 @@
-# Problem Description:
-# Given an integer array A of non-negative integers representing an elevation map
-# where the width of each bar is 1, compute maximum water it is able to trap after raining.
-
+"""
+Problem Description:
+Given an integer array A of non-negative integers representing an elevation map
+where the width of each bar is 1, compute maximum water it is able to trap after raining.
+"""
 
 # Function to calculate the maximum water that can be trapped .
 def max_water(arr, n):
 
     temp = 0
-
     # iterate the array
     for i in range(1, n - 1):
 
@@ -30,35 +30,33 @@ def max_water(arr, n):
 
 if __name__ == "__main__":
     arr = []
-
     print("Enter number of elements")
     # number of elements as input
     n = int(input())
     
     print("Enter the elements")
     # iterating till the range
-    for i in range(0,n):
+    for i in range(0, n):
         p = int(input())
         arr.append(p)
-
     print(max_water(arr, n))
 
+"""
+Test case 1:
+    input:
+    n=5
+    arr=[2,0,2,3,4]
+    output:
+    2
 
-# Test case 1:
-#     input:
-#     n=5
-#     arr=[2,0,2,3,4]
-#     output:
-#     2
+Test case 2:
+    input:
+    n=7
+    arr=[5,1,2,3,2,6,1]
+    output:
+    12
 
-# Test case 2:
-#     input:
-#     n=7
-#     arr=[5,1,2,3,2,6,1]
-#     output:
-#     12
+Time Complexity: O(n)
 
-
-# Time Complexity: O(n)
-
-# Space Complexity: O(1)
+Space Complexity: O(1)
+"""
