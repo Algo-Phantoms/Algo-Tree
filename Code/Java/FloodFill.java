f0b5a631347a43e4b45f173121e11bc6fca695d1
Nidhi Anil Khare <55029966+nidhi-08@users.noreply.github.com>
2021-04-25 11:36:09 +0530

    Update FloodFill.java

--- Code/Java/FloodFill.java
+++ Code/Java/FloodFill.java
@@ -15,7 +15,9 @@ import java.util.*;
 
 class FloodFill {
     public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        //store current colour
         int color = image[sr][sc];
+        //if new colour is different from current, call dfs to change colour
         if (color != newColor) {
             dfs(image, sr, sc, color, newColor);
         }
@@ -23,11 +25,15 @@ class FloodFill {
     }
     
     public static void dfs(int[][] image, int sr, int sc, int color, int newColor) {
+        //Base Cases
         if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) {
             return;
         }
+        //Replace colour at cell (sr,sc)
         image[sr][sc] = newColor;
-        dfs(image, sr + 1, sc, color, newColor);    //recursive calls
+        
+        //recursive calls for adjacent above, below, left and right
+        dfs(image, sr + 1, sc, color, newColor);
         dfs(image, sr - 1, sc, color, newColor);
         dfs(image, sr, sc + 1, color, newColor);
         dfs(image, sr, sc - 1, color, newColor);
@@ -35,22 +41,33 @@ class FloodFill {
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        //accpeting the grid 
         System.out.println("Enter length and breadth of grid: ");
         int length = sc.nextInt();
         int breadth = sc.nextInt();
-        int grid[][] = new int[length][breadth];
+        int grid[][] = new int[length][breadth]; 
+        
+        //accepting the colours
         System.out.println("Enter colours of the grid (integers): ");
         for(int i=0;i<length;i++){
             for(int j=0;j<breadth;j++){
                 grid[i][j] = sc.nextInt();
             }
         } 
+        
+        //accepting the starting point
         System.out.println("Enter starting cell co-ordinates: ");
         int row = sc.nextInt();
         int cols = sc.nextInt();
+        
+        //new colour
         System.out.println("Enter new colour number: ");
         int colour = sc.nextInt();
+        
+        //calling flood fill function on the grid
         int[][] result = floodFill(grid, row, cols, colour);
+        
+        //output final result
         System.out.println("Final color-changed grid: ");
         for(int i=0;i<result.length;i++){
             for(int j=0;j<result[0].length;j++){
@@ -64,7 +81,8 @@ class FloodFill {
 
 /**
  * OUTPUT:
- * Enter length and breadth of grid: 
+Case 1: 
+Enter length and breadth of grid: 
 3 3
 Enter colours of the grid (integers): 
 1 1 1
@@ -78,6 +96,33 @@ Final color-changed grid:
 2   2   2
 2   2   0
 2   0   1
+
+Case 2: 
+Enter length and breadth of grid: 
+8 8 
+Enter colours of the grid (integers): 
+3 2 1 1 1 1 1 1
+1 1 1 1 1 1 0 0 
+1 0 0 1 1 0 1 1
+1 2 2 2 2 0 1 0
+1 1 1 0 2 2 1 0
+1 1 1 2 2 2 2 0
+1 1 1 1 1 2 1 1
+1 1 1 1 1 2 2 1
+Enter starting cell co-ordinates: 
+4 4
+Enter new colour number: 
+3
+Final color-changed grid: 
+3	2	1	1	1	1	1	1	
+1	1	1	1	1	1	0	0	
+1	0	0	1	1	0	1	1	
+1	3	3	3	3	0	1	0	
+1	1	1	3	3	0	1	0	
+1	1	1	3	3	3	3	0	
+1	1	1	1	1	3	1	1	
+1	1	1	1	1	3	3	1	
+
  * 
  * 
  * 
