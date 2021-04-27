e9609384095372ceb49760033a8553f30f55a775
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-04-27 08:24:56 +0530

    Update peakvalueinarray.java

--- Code/Java/peakvalueinarray.java
+++ Code/Java/peakvalueinarray.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class peakvalueinarray {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Size of Arraay");
+        System.out.println("Enter Size of Array");
         int n = scan.nextInt();
         if (n>0) {
             System.out.println("Enter " + n + " Elements of array");
