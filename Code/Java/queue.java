3fb9b3c469547d89792f8b6744ce7d4f3a716657
Chirag018 <chiragmmehta16@gmail.com>
2021-04-30 10:06:59 +0530

    Update README.md file

---
+++ Code/Java/queue.java
@@ -0,0 +1,143 @@
+/*
+ *                    Implementation of Queue
+ *         
+ * Queue is a linear structure which follows a particular order in 
+ * which the operations are performed. 
+ * The order is First In First Out (FIFO).
+ */
+import java.util.*;
+class Queue {
+    int front, rear, size;
+    int capacity;
+    int array[];
+
+    public Queue(int capacity)
+    {
+        this.capacity = capacity;
+        front = this.size = 0;
+        rear = capacity - 1;
+        array = new int[this.capacity];
+    }
+
+    boolean isFull(Queue queue)
+    {
+        return (queue.size == queue.capacity);
+    }
+
+    boolean isEmpty(Queue queue)
+    {
+        return (queue.size == 0);
+    }
+   // Pushing element into the Queue
+    void enqueue(int item)
+    {
+        if (isFull(this))
+            return;
+        this.rear = (this.rear + 1)        % this.capacity;
+        this.array[this.rear] = item;
+        this.size = this.size + 1;
+        System.out.println(item
+            + " enqueued to queue");
+    }
+    
+    // Popping element from the Queue
+    int dequeue()
+    {
+        if (isEmpty(this))
+            return Integer.MIN_VALUE;
+
+        int item = this.array[this.front];
+        this.front = (this.front + 1)        % this.capacity;
+        this.size = this.size - 1;
+        return item;
+    }
+    
+    // Function returns front/top element from the Queue
+    int front()
+    {
+        if (isEmpty(this))
+            return Integer.MIN_VALUE;
+
+        return this.array[this.front];
+    }
+    
+    // Function returns rear element from the Queue
+    int rear()
+    {
+        if (isEmpty(this))
+            return Integer.MIN_VALUE;
+
+        return this.array[this.rear];
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        Queue queue = new Queue(1000);
+        System.out.println("No. of elements to be added in Queue :");
+        int n=in.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int input=in.nextInt();
+            queue.enqueue(input);
+
+        }
+
+        System.out.println("Front item is "
+            + queue.front());
+        System.out.println(queue.dequeue()
+            + " dequeued from queue\n");
+        System.out.println("Front item is "
+            + queue.front());
+
+        System.out.println("Rear item is "
+            + queue.rear());
+
+            in.close();
+    }
+}
+
+/*
+ * Test Case 1 :
+ * Input : No. of elements to be added in Queue :
+ * 5 
+ * 1 2 3 4 5
+ * Output :
+ * 1 enqueued to queue
+   2
+   2 enqueued to queue
+   3
+   3 enqueued to queue
+   4
+   4 enqueued to queue
+   5
+   5 enqueued to queue
+   
+   Front item is 1
+   1 dequeued from queue
+
+   Front item is 2
+   Rear item is 5
+   
+   Test Case 2 :
+ * Input : No. of elements to be added in Queue :
+ * 3 
+ * 1 2 3 
+ * Output :
+ * 1 enqueued to queue
+   2
+   2 enqueued to queue
+   3
+   3 enqueued to queue
+  
+   Front item is 1
+   1 dequeued from queue
+
+   Front item is 2
+   Rear item is 3
+   
+ * Time  Complexity is
+ * Insertion(Enqueue) O(1) 
+ * Deletion(Dequeue)  O(1) 
+ * Space Complexity is O(n) , n is the size of the Queue.
+ */
