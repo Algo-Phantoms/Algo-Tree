7a9bc595eaf08ad7d8b78fe6bfed02ce6a0ba9ad
AkanshaUniyal <78859329+AkanshaUniyal@users.noreply.github.com>
2021-05-01 11:28:07 +0530

    Update transpose.java

--- 2D_Array/transpose.java
+++ 2D_Array/transpose.java
@@ -16,19 +16,19 @@ public class transpose
            System.out.println("Enter the order of square matrix");
            order=scan.nextInt();
 
-           int [][] matrix=new int[order][order];
+           int [][] matrix = new int[order][order];
 	   System.out.println("Enter the elements of the matrix");
-	   for(int row=0;row<order;row++)
+	   for(int row=0; row < order; row++)
 	   {
-		for(int column=0;column<order;column++)
+		for(int column=0; column < order; column++)
 		{
 		     matrix[row][column]=scan.nextInt();
 		}
 	   }
 
-           for(int count=1;count<order;count++)
+           for(int count=1; count < order; count++)
            {
-		for(int count1=0;count1<=count;count1++)
+		for(int count1=0; count1 <= count; count1++)
 		{
                	      swap=matrix[count][count1];
                	      matrix[count][count1]=matrix[count1][count];
@@ -36,9 +36,9 @@ public class transpose
 		}
            }
 	   System.out.println("Transpose:");
-	   for(int row1=0;row1<order;row1++)
+	   for(int row1=0; row1 < order; row1++)
            {
-                for(int column1=0;column1<order;column1++)
+                for(int column1=0; column1 < order; column1++)
                 {
                       System.out.print(matrix[row1][column1]+" ");
                 }
@@ -47,17 +47,19 @@ public class transpose
         
        }
 }
-/*        Output:
+/*  
 Test Case 1:
 
 Enter the order of square matrix
 5
+
 Enter the elements of the matrix
 23 43 89 67 5
 34 54 56 7 89
 57 96 42 87 9
 79 34 32 1 23
-46 7 78 3 65
+46 7 78 3 65 
+
 Transpose:
 23 34 57 79 46
 43 54 96 34 7
@@ -65,20 +67,23 @@ Transpose:
 67 7 87 1 3
 5 89 9 23 65
 
+
 Test Case 2:
 
 Enter the order of square matrix
 3
+
 Enter the elements of the matrix
 34 87 58
 33 5 6
 563 65 5
 
+Transpose: 
 34 33 563
 87 5 65
 58 6 5
 
-Time Complexity: O(n2)
+Time Complexity: O(order^2)
 Space Complexity: O(1)
 */
 
