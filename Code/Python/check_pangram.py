93a62d19da5fd50cfe8d02a4140e6b5ebcd696a5
Samya Jain <74755065+samya02@users.noreply.github.com>
2021-05-02 23:52:29 +0530

    Made the required changes!

--- Code/Python/check_pangram.py
+++ Code/Python/check_pangram.py
@@ -1,7 +1,10 @@
-#Program to check if a given string is pangram or not
+'''
 
-#A pangram is a sentence containing every letter in the English Alphabet.
+Program to check if a given string is pangram or not
 
+A pangram is a sentence containing every letter in the English Alphabet.
+
+'''
 
 #importing the string module
 from string import *
@@ -23,11 +26,11 @@ def pangram(string):
 
 
 #main
-string=input("Enter any string: ")
+string = input("Enter any string: ")
 
 
 #Function call
-if(pangram(string)==True):
+if(pangram(string) == True):
     
     print("\n The given string is a pangram")
           
@@ -35,12 +38,16 @@ else:
     print("\n The given string is not a pangram")
 
 
-    '''Test cases: 
-1) I/P:The Quick Brown Fox Jumps Over The Lazy Dog
-   O/P:The given string is a pangram
+    
+''' 
+Test cases: 
 
-2) I/P:2brown5! (including special characters and digits)
-   O/P:The given string is not a pangram
-'''
+1) Input: The Quick Brown Fox Jumps Over The Lazy Dog
+   Output: The given string is a pangram
 
-#Time complexity:O(n)
+2) Input: 2brown5! #(including special characters and digits)
+   Output: The given string is not a pangram
+
+
+Time complexity: O(n)
+Space Complexity : O(1)
