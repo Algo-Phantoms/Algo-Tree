8add3995f33891f084b72423c670a257a126a6a4
Neha Kumari <47255445+neha07kumari@users.noreply.github.com>
2021-05-06 23:32:01 +0530

    Update distance_between_two_node_of_a_BT.cpp

--- Tree/distance_between_two_node_of_a_BT.cpp
+++ Tree/distance_between_two_node_of_a_BT.cpp
@@ -35,7 +35,7 @@ Node* LCA(Node* root, int n1,int n2){
     Node* left=LCA(root->left,n1,n2);
     Node* right=LCA(root->right,n1,n2);
 
-    if(left!=NULL && right!=NULL){
+    if(left!=NULL && right != NULL){
         return root;
     }
 
