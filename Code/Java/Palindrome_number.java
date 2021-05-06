0e9fe9bbacf2eebe022643eea0a5cd5a72570e64
Tejal18 <62290139+Tejal18@users.noreply.github.com>
2021-05-06 19:19:10 +0530

    Updated Palindrome code (java)

--- Code/Java/Palindrome_number.java
+++ Code/Java/Palindrome_number.java
@@ -1,9 +1,8 @@
-/* Check for Palindrome Number */
-/* To check whether the number is palindrome or not ,  
- * check if the number read backwards is equal to the original number if equal then,
- *  it will be considered to be a palindrome number else it is not a palindrome number.*/
-
-/* Algorithm :
+/* Check for Palindrome Number 
+To check whether the number is palindrome or not ,  
+check if the number read backwards is equal to the original number if equal then,
+it will be considered to be a palindrome number else it is not a palindrome number.
+Algorithm :
 Step 1:Get the number to check for palindrome
 Step 2: Hold the number in temporary variable (temp)
 Step 3: Reverse the number
@@ -15,47 +14,51 @@ package palin;
 import java.util.*;
 class P
 {
-	boolean Panlindrome (int n)
+	boolean Palindrome (int n)
 	{
-		int rem=0;  //for remainder
+		//for remainder
+		int rem=0;  
 		int sum=0;  
 		int temp=0;
-		temp=n;   //assign n to temp
-		while(n>0){  //loop until n is greater than 0
-			rem= n % 10;   //extract the remainder to get the last digit
+		//assign n to temp
+		temp=n; 
+		//loop until n is greater than 0
+		while(n>0){ 
+			//extract the remainder to get the last digit
+			rem= n % 10;   
 			sum = (sum*10)+rem; 
-			n = n/10;   //divide the number by 10
+			//divide the number by 10
+			n = n/10;   
 		}
-		if(sum==temp) {   //original number is equal to reversed number 
-			return true;  //true if number is a palindrome
+		//original number is equal to reversed number
+		if(sum==temp) {
+			//true if number is a palindrome
+			return true;  
 		}
 		else 
 		{
-			return false; //false if number is not a palindrom
+			//false if number is not a palindrome
+			return false; 
 		}
 	}
 }
-public class Pmain {
+public class Palindrome_number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		boolean result; 
 		int n1=0;
-		P pa = new P();  //create object of class P
+		//create object of class P
+		P pa = new P();  
 		System.out.println("-------To check whether a number is palindrome or not-------");
 		System.out.println("Enter a number: ");
 		n1 = sc.nextInt();
 		System.out.println("Input: "+n1);
-		result= pa.Panlindrome(n1);
+		result= pa.Palindrome(n1);
 		System.out.println("Output: "+result);
 	}
 }
-/*The time complexity to check if a number is a palindrome or not is  O(log10​(n)), n is the input number
- * Because we are dividing the number by 10 in every iteration. 
- * So the time complexity can be said is equal to the number of digits in a number.*/
-
-/* Test case 1: 
+/*Test case 1: 
 -------To check whether a number is palindrome or not------- 
 Enter a number: 
 124
@@ -69,6 +72,9 @@ Enter a number:
 Input: 656
 Output: true
 
+Time Complexity: O(log10​(n)) where n is the input number
+Because we are dividing the number by 10 in every iteration. 
+So the time complexity can be said is equal to the number of digits in a number.
 Space complexity : O(1) //As Only the number is required to be stored. 
 */
 
