4ddad633673cd310ed06622fe7f5e1ed026399b4
SHIVAM KUMAR <sosmart08642@gmail.com>
2021-05-13 10:48:09 +0530

    Update negative_cycle_in_a_graph.py
    All the changes are done

--- Code/Python/negative_cycle_in_a_graph.py
+++ Code/Python/negative_cycle_in_a_graph.py
@@ -1,33 +1,25 @@
-
-# Program to find the negative cycle in the graph
-# Here BellmanFord's Algorithm is used to find the negative cycle in Graph
-
-# Time Complexity: O(V*E) 
-# Space Complexity : O(V)
-
+'''
+Program to find the negative cycle in the graph
+Here BellmanFord's Algorithm is used to find the negative cycle in Graph
+'''
 class Edge:
 	def __init__(self):
 		self.src = 0
 		self.dest = 0
 		self.weight = 0
 
-# Structure to represent a directed
-# and weighted graph
+# Structure to represent a directed and weighted graph
 class Graph:
-
 	def __init__(self):
 		
-		# V. Number of vertices, E.
-		# Number of edges
+		# V. Number of vertices, E - Number of edges
 		self.V = 0
 		self.E = 0
 		
-		# Graph is represented as
-		# an array of edges.
+		# Graph is represented as an array of edges.
 		self.edge = []
 	
-# Creates a new graph with V vertices
-# and E edges
+# Creates a new graph with V vertices and E edges
 def createGraph(V, E):
 	graph = Graph()
 	graph.V = V
@@ -35,61 +27,49 @@ def createGraph(V, E):
 	graph.edge = [Edge() for i in range(graph.E)]
 	return graph
 
-# Function runs Bellman-Ford algorithm
-# and prints negative cycle (if present)
+# Function runs Bellman-Ford algorithm and prints negative cycle (if present)
 def NegativeCycleBellmanFord(graph, src):
 	V = graph.V
 	E = graph.E
 	dist =[1000000 for i in range(V)]
 	parent =[-1 for i in range(V)]
 	dist[src] = 0
-
 	# Relax all edges |V| - 1 times.
 	for i in range(1, V):
 		for j in range(E):
-	
 			u = graph.edge[j].src
 			v = graph.edge[j].dest
 			weight = graph.edge[j].weight
-
 			if (dist[u] != 1000000 and
 				dist[u] + weight < dist[v]):
-			
 				dist[v] = dist[u] + weight
 				parent[v] = u
-
+				
 	# Check for negative-weight cycles
 	isNeg = -1
 	for i in range(E):
 		u = graph.edge[i].src
 		v = graph.edge[i].dest
 		weight = graph.edge[i].weight
-
 		if (dist[u] != 1000000 and
 			dist[u] + weight < dist[v]):
-			
-			# Store one of the vertex of
-			# the negative weight cycle
+			# Store one of the vertex of the negative weight cycle
 			isNeg = v
 			break
 		
 	if (isNeg != -1):	
 		for i in range(V):	
 			isNeg = parent[isNeg]
-
 		# To store the cycle vertex
 		cycle = []	
 		v = isNeg
-		
 		while (True):
 			cycle.append(v)
 			if (v == isNeg and len(cycle) > 1):
 				break
 			v = parent[v]
-
 		# Reverse cycle[]
 		cycle.reverse()
-
 		# Printing the negative cycle
 		for v in cycle:	
 			print(v, end = " ");			
@@ -100,18 +80,12 @@ def NegativeCycleBellmanFord(graph, src):
 
 # Driver Code
 if __name__=='__main__':
-	
 	# Number of vertices in graph
 	V = int(input())
-
 	# Number of edges in graph
 	E = int(input())
 	graph = createGraph(V, E)
-
-	# Given Graph
-	
-    # Format Of input
-    # src dest weight
+	# Given Graph Format Of input src dest weight
 	for i in range(E):
 	    (src, dest, weight) = map(int, input().split())
 	    graph.edge[i].src = src
@@ -119,8 +93,10 @@ if __name__=='__main__':
 	    graph.edge[i].weight = weight
 	# Function Call
 	NegativeCycleBellmanFord(graph, 0)
-
 ''' 
+Time Complexity: O(V*E) 
+Space Complexity : O(V)
+
 Test Case #1
     Input
     5
@@ -130,10 +106,10 @@ Test Case #1
     2 3 3
     3 4 -3
     4 1 -4
-
+    
     Output
     1 2 3 4 1 
-
+    
 Test case #2
     Input
     5
@@ -146,7 +122,6 @@ Test case #2
     
     Output
     No negative cycle in Graph
-
 '''
 
 
