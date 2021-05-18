b1e649cc7bb36dbfbcfd452d3e513fecb8b6b641
Ritika Verma <76901399+ansi22@users.noreply.github.com>
2021-05-18 20:51:08 +0530

    Update longest_prefix_suffix.cpp

--- Code/C++/longest_prefix_suffix.cpp
+++ Code/C++/longest_prefix_suffix.cpp
@@ -8,48 +8,51 @@
 -otherwise check for the shorter length
  */
 
- #include <iostream>
- using namespace std;
+#include <iostream>
+using namespace std;
 
- int main() {
+int main()
+{
 
- string s;
- //input from the user
- cin>>s;
- /*if length of given string is less
- than two, no such substring exist
- hence prints 0*/
- if(s.length()<2)
-     cout<<0;
- else{
-     /*divide string into two equal parts
-     and store starting indices of both*/
-     int l=0,i=s.length()/2;
-     /*loop calculating the length of
-     such substring*/
-     while(i<s.length())
-     {
-    /*check if left string character is equal
-     to right one if so increament indices of both by 1*/
-     if(s[i]==s[l])
-     {
-     ++l;
-     ++i;
-     }
-    /*otherwise check for shoter substring*/
-     else
-     {
-     i=i-l+1;
-     l=0;
-     }
-     }
-    /*as overlapping is not allowed*/
- if(l>s.length()/2) 
-      cout<<s.length()/2 ;
- else 
-      cout<<l;
-}
-return 0;
+    string s;
+    // input from the user
+    cin>>s;
+    /*if length of given string is less
+    than two, no such substring exist
+    hence prints 0*/
+    if (s.length()<2)
+        cout<<0;
+    else 
+    {
+        /*divide string into two equal parts
+        and store starting indices of both*/
+        int l=0, i=s.length()/2;
+        /*loop calculating the length of
+        such substring*/
+        while (i<s.length()) 
+        {
+        /*check if left string character is equal
+          to right one if so increament indices of both
+          by 1*/
+            if (s[i]==s[l])
+            {
+                ++l;
+                ++i;
+            }
+        /*otherwise check for shoter substring*/
+            else 
+            {
+                i=i-l+1;
+                l=0;
+            }
+        }
+        /*as overlapping is not allowed*/
+        if (l>s.length()/2)
+            cout<<s.length()/2;
+        else
+            cout<<l;
+    }
+    return 0;
 }
 
 
