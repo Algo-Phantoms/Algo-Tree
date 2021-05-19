c2b98dba43a05e86ffbd9b5ffa2fa406296b5c10
Sushant Gaurav <sushantgaurav57@gmail.com>
2021-05-19 19:07:33 +0530

    Update unsorted_zig_zag_array.cpp

--- Code/C++/unsorted_zig_zag_array.cpp
+++ Code/C++/unsorted_zig_zag_array.cpp
@@ -1,23 +1,25 @@
+/*
+Approach : Swap pairs of elements except the first element.
+As - keep a[0], swap a[1] with a[2], swap a[3] with a[4], and so on.
+So, we will take array and its size as parameters, run a loop from i to size of the array 
+and swap every pair of element using swap function. At the end of loop, we will print the array.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-Approach :
-Exclude the first element, swap the remaining elements in pairs.
-As - keep a[0], swap a[1] and a[2], swap a[3] and a[4], and so on.
-*/
-
+//helper function that converts given array in zig-zag form
 void zigzag(int a[], int n)
 {
-    for(int i=1 ; i<n ; i+=2)
+    for(int i = 1 ; i < n ; i += 2)
     {
         if(i < n-1)
             swap(a[i], a[i+1]);
     }
 
-    cout<<"\nZig - Zag array : ";
-    for(int i=0 ; i<n ; i++)
-        cout<<a[i]<<"  ";
+    cout << "\nZig - Zag array : ";
+    for(int i=0 ; i < n ; i++)
+        cout << a[i] << "  ";
 }
 
 int main()
@@ -25,9 +27,9 @@ int main()
     int a[] = {4,3,7,8,6,2,1};
     int n = sizeof(a)/sizeof(a[0]);
 
-    cout<<"Original array  : ";
-    for(int i=0 ; i<n ; i++)
-        cout<<a[i]<<"  ";
+    cout << "Original array  : ";
+    for(int i = 0 ; i < n ; i++)
+        cout << a[i] << "  ";
 
     zigzag(a, n);
 
@@ -37,10 +39,13 @@ int main()
 /*
     Test Case -
 
-    Output :
-
+    Sample Input/Output (1):
     Original array  : 4  3  7  8  6  2  1
     Zig - Zag array : 4  7  3  6  8  1  2
+    
+    Sample Input/Output (2):
+    Original array  : 14  13  17  18  16  12  11
+    Zig - Zag array : 14  17  13  16  18  11  12
 
     Time Complexity : O(n)
     Space Complexity : O(1)
