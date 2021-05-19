bf4ca19ff0b80e904c2bdcb44c2ad176539a430a
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-05-19 21:46:32 +0530

    Update palindrome_number.py

--- Code/Python/palindrome_number.py
+++ Code/Python/palindrome_number.py
@@ -7,23 +7,26 @@
 #                The given number is a palindrome.
 
 #Input
-n=int(input("Enter the number\n"))
-t=n
-rev_no=0
-x=0
+
+n = int(input("Enter the number\n"))
+t = n
+rev_no = 0
+x = 0
 
 #through the following loop we will find the reverse of the given number
-while(n!=0):
-      c=n%10
-      rev_no=(rev_no*10) + (n%10)
-      n=n//10
+
+while n != 0:
+      c = n%10
+      rev_no = (rev_no*10) + (n%10)
+      n = n//10
      
-#if the given number is equal to its reverse then the given number is a palindrome    
-if t==rev_no:
+#if the given number is equal to its reverse then the given number is a palindrome   
+
+if t == rev_no:
       print("The given number is a palindrome.")
 else:
       print("The given number is not a palindrome.")
-     
+
 #         TEST CASES
 #      
 #     1)INPUT:
