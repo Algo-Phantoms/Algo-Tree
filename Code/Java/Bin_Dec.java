b5cbb022205a7d5d7b6aa9c3286abdf3c9bcd4b2
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-05-22 19:36:48 +0530

    Description added

--- Code/Java/Bin_Dec.java
+++ Code/Java/Bin_Dec.java
@@ -1,5 +1,15 @@
 /*
+Idea is same which is used all over the world for transforming from binary to decimal
+The idea is to extract the digits of a given binary number starting from the rightmost digit
+and keep a variable dec. At the time of extracting digits from the binary number,
+multiply the digit with the (Power of 2) that will be made via Math.pow in which increment of i
+will increase the power as we proceed to move towards digit at first place and add it to the variable
+dec. In the end, the variable dec will store the required decimal number.
 
+For Example:
+If the binary number is 10110.
+If we move from last digit to the first one
+dec = 0*(2^0) + 1*(2^1) + 1*(2^2) + 0*(2^3) + 1*(2^4) = 22
  */
 package binTOdecimal;
 import java.util.Scanner;
