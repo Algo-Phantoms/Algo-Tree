d3f807af9c1791f9161e4a11e92d8100d9e526b0
ShriKrushna Bhagwat <krish30jan@gmail.com>
2021-05-22 19:28:57 +0530

    Code Added

--- Code/Java/Bin_Dec.java
+++ Code/Java/Bin_Dec.java
@@ -7,9 +7,22 @@ import java.util.Scanner;
 public class BinaryToDecimal {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter No : ");
+        System.out.println("Enter Binary Number :");
         int n = scan.nextInt();
-        System.out.println(isPrime(n));
+        System.out.println("Decimal Representation is :");
+        bintodec(n);
+    }
+    public static void bintodec(int n){
+        int dec = 0;
+        int dummy = n;
+        int count = 0;
+        while (dummy > 0){
+            int lastdigit = dummy % 10;
+            dummy /= 10;
+            dec += (lastdigit * Math.pow(2,count));
+            count++;
+        }
+        System.out.println(dec);
     }
 }
 /*
