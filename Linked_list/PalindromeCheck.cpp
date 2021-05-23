71ad18a42725a1169ab25b0fbc1059c60ee33f66
AnushkaS09 <67291868+AnushkaS09@users.noreply.github.com>
2021-05-24 02:45:38 +0530

    Update PalindromeCheck.cpp

--- Linked_list/PalindromeCheck.cpp
+++ Linked_list/PalindromeCheck.cpp
@@ -1,7 +1,20 @@
    /*Check if the linked list is palindrome or not if yes return true if not return false. Linked list has to be taken as an input from the user.*/
 
+#include <iostream>
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
 
-       Node* reverse (Node *save)
+using namespace std;
+Node* reverse (Node *save)
        {
           if(save == NULL || save->next == NULL)  
            {
@@ -34,6 +47,47 @@
                head = head->next;
            }
        }
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		bool ans = isPalindrome(head);
+
+		if (ans) cout << "true";
+		else cout << "false";
+
+		cout << endl;
+	}
+
+	return 0;
+}
 /*
 ----------------------------------------------------------------------------------
 SAMPLE TEST CASE 1:
@@ -51,6 +105,6 @@ SAMPLE TEST CASE 2:
  1 2 3 -1
  false
  ----------------------------------------------------------------------------------------
-TIME COMPLEXITY: O(N)
+TIME COMPLEXITY: O(N) [WHERE N IS THE SIZE OF SINGLY LINKED LIST]
 SPACE COMPLEXITY: O(1)
 */
