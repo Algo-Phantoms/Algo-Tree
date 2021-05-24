9d669d97bb304330e65e4ce89a69d39bff16794d
Amit kumar mishra <akm43300@gmail.com>
2021-05-24 21:12:01 +0530

    Update'

--- Code/Python/Kth_Minimum_in_array.py
+++ Code/Python/Kth_Minimum_in_array.py
@@ -13,11 +13,44 @@ def kthSmallest(arr, n, k):
 if __name__=='__main__':
     arr = []
     n = int(input("Enter number of elements: "))
+    print("Enter elements: ")
     for i in range(n):
-        element = int(input("Enter element: "))
+        element = int(input())
         arr.append(element)
 
     k = int(input("Enter the number k: "))
     print("K'th smallest element is",
           kthSmallest(arr, n, k))
- 
+
+'''
+Time Complexity: O(N Log N)
+Space Complexity: O(N)
+
+Example 1:
+Input:
+Enter number of elements: 
+6
+Enter elements: 
+7 10 4 3 20 15
+
+Enter the number k: 
+3
+
+Output:
+K'th smallest element is:
+7
+
+Example 2:
+Input:
+Enter number of elements: 
+7
+Enter elements: 
+8 1 0 9 5 11 4
+
+Enter the number k: 
+4
+
+Output:
+K'th smallest element is:
+5
+'''
