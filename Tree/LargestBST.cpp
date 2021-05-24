d469b3d280907b4f54ffc8e528e8228d4859e49a
AnushkaS09 <67291868+AnushkaS09@users.noreply.github.com>
2021-05-24 12:15:52 +0530

    Update LargestBST.cpp

--- Tree/LargestBST.cpp
+++ Tree/LargestBST.cpp
@@ -4,9 +4,9 @@
 #include <queue>
 #include<limits.h>
 
-template <typename T> // template to take any data type string and int as integer
-class BinaryTreeNode {
-   public:
+template <typename T>    // template to take any data type string or integer as input
+class BinaryTreeNode {   
+   public:               //following is the binary tree node class structure
     T data;
     BinaryTreeNode<T>* left;
     BinaryTreeNode<T>* right;
@@ -25,45 +25,46 @@ class BinaryTreeNode {
 using namespace std;
 
 
-int height(BinaryTreeNode<int> *root)  //returnning heigth of the tree
+int height(BinaryTreeNode<int> *root)  //returning heigth of the tree
 
-    if(root == NULL) return 0;
-    return 1 + max( height(root->left), height(root->right) ); 
+    if(root == NULL) return 0;                                   // height of the tree will be the height of left or right child node with maximum height
+    return 1 + max( height(root->left), height(root->right) );  //  and + 1 for the root to child height
 }
 
-bool isbst(BinaryTreeNode<int> *root, int min, int max){ //checking if the tree is bst or not
+bool isbst(BinaryTreeNode<int> *root, int min, int max){       //checking if the tree is bst or not
     if(root == NULL) return true;
     if(root->data < min || root->data > max) return false;
-    return isbst(root->left, min, root->data-1) && isbst(root->right, root->data, max); 
+    return isbst(root->left, min, root->data-1) && isbst(root->right, root->data, max);    //checking BST for right child of root root and left child of root by
+                                                                                          // giving a recursive call if both are true only then the root is BST   
 }
 bool isBST(BinaryTreeNode<int> *root){
 
-    return isbst(root, INT_MIN, INT_MAX); //creating helper function to check BST
+    return isbst(root, INT_MIN, INT_MAX);  //creating helper function to check BST
 }
 
 int largestBSTSubtree(BinaryTreeNode<int> *root) {
     
     if(isBST(root)) return height(root);
-    return max(largestBSTSubtree(root->left), largestBSTSubtree(root->right));
+    return max(largestBSTSubtree(root->left), largestBSTSubtree(root->right));    //determining which is a larger BST Subtree by finding the the height of left and right subtree of root
    
 }
 
 BinaryTreeNode<int>* takeInput() { //INPUT FUNCTION
     int rootData;
-    cout<<"enter the root data:";
+    cout << "enter the root data:";
     cin >> rootData;
     if (rootData == -1) {
         return NULL;
-    }
+    } 
     BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
-    queue<BinaryTreeNode<int>*> q;
+    queue<BinaryTreeNode<int>* > q;
     q.push(root);
-    while (!q.empty()) {
-        BinaryTreeNode<int>* currentNode = q.front();
-        q.pop();
+    while ( !q.empty() ) {           // Taking input data and pushing it in queue "q". 
+        BinaryTreeNode<int>* currentNode = q.front();   
+        q.pop();                  // Pushing data in "q" in the form [(ROOT) A, (LEFT CHILD OF ROOT) B, (RIGHT CHILD OF ROOT) C, LEFT CHILD Of B, RIGHT CHILD OF B, LEFT CHILD of C,..]
         int leftChild, rightChild;
        
-        cin >> leftChild;
+        cin >> leftChild;     
         if (leftChild != -1) {
             BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
             currentNode->left = leftNode;
