ef7ac7078842c14621b5a0f37ca3afdddc37b6f9
ssaumyaa7 <64359826+ssaumyaa7@users.noreply.github.com>
2021-05-26 17:51:02 +0530

    commit changes

--- Code/Java/Longest_Common_Prefix.java
+++ Code/Java/Longest_Common_Prefix.java
@@ -8,31 +8,31 @@ import java.util.Scanner;
 
 public class Main {
     private static String longestCommonPrefix(String[] strs) {
-        String lcp="";
+        String lcp = "";
         
-        if(strs.length==0 || strs==null){
+        if(strs.length == 0 || strs == null){
             return lcp;
         }
         
-        int index=0;
+        int index = 0;
         
         for(char c:strs[0].toCharArray())
         {
             for(int i=1;i<strs.length;i++)
             {
-                if(index>= strs[i].length() || c!=strs[i].charAt(index)){
+                if(index >= strs[i].length() || c != strs[i].charAt(index)){
                     return lcp;
                 }
             }
-            lcp+=c;
+            lcp += c;
             index++;
         }
         return lcp;
         
     }
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        String str= sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         String[] words = str.split("\\s+");
 		System.out.println(longestCommonPrefix(words));
 	}
