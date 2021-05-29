c24e98d9d45c8a3b9602aea644d47b8862436e71
ssaumyaa7 <64359826+ssaumyaa7@users.noreply.github.com>
2021-05-29 13:51:50 +0530

    alligned and spaces provided

--- Code/Java/Longest_Common_Prefix.java
+++ Code/Java/Longest_Common_Prefix.java
@@ -10,7 +10,8 @@ public class Main {
     private static String longestCommonPrefix(String[] strs) {
         String lcp = "";
         
-        if(strs.length == 0 || strs == null){
+        if(strs.length == 0 || strs == null)
+	{
             return lcp;
         }
         
@@ -18,7 +19,7 @@ public class Main {
         
         for(char c : strs[0].toCharArray())
         {
-            for(int i=1 ; i<strs.length ; i++)
+            for(int i = 1; i < strs.length; i++)
             {
                 if(index >= strs[i].length() || c != strs[i].charAt(index))
 		{
@@ -31,27 +32,26 @@ public class Main {
         return lcp ;
         
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String[] words = str.split("\\s+");
-		System.out.println(longestCommonPrefix(words));
-	}
+	System.out.println(longestCommonPrefix(words));
+    }
 }
 
 /*
 Time complexity:
-O(N) as depends on the characters given in the string
+	O(N) as depends on the characters given in the string
 
 Space complexity:
-Takes constant space.
+	Takes constant space.
 */
 
 /*
-
-    Test case:
-    
-        Input:  small smile smell
+Test cases:
+	Input:  small smile smell
         Output: sm
 	
 	Input:  flight flock fly
