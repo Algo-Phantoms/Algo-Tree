16df498ab2ef2f9dc328df0e3383a2a20809db6e
Rishabh Singhal <Rishabh.singhal@research.iiit.ac.in>
2021-06-01 20:22:16 +0530

    Update knapsack_dp.py

--- Code/Python/knapsack_dp.py
+++ Code/Python/knapsack_dp.py
@@ -27,7 +27,7 @@ def knapsack(a, val, n, w):
     for i in range(1, n + 1):
         for j in range(1, w + 1):
             if j >= a[i - 1]:
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j-a[i - 1]]+val[i - 1])
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - a[i - 1]] + val[i - 1])
             else:
                 dp[i][j] = dp[i - 1][j]
 
